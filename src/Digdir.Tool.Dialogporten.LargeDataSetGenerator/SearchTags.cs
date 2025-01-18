3cf2065d4239a459a2cf5f272dda27b516a48e30
Ole Jørgen Skogstad <skogstad@softis.net>
2025-01-18 14:07:30 +0100

    wip [skip ci]

--- src/Digdir.Tool.Dialogporten.LargeDataSetGenerator/SearchTags.cs
+++ src/Digdir.Tool.Dialogporten.LargeDataSetGenerator/SearchTags.cs
@@ -15,9 +15,23 @@ internal static class SearchTags
         var searchTagId2 = DeterministicUuidV7.Generate(dto.Timestamp, nameof(DialogSearchTag), 2);
         var searchTagId3 = DeterministicUuidV7.Generate(dto.Timestamp, nameof(DialogSearchTag), 3);
 
-        searchTagCsvData.AppendLine($"{searchTagId1},This is a search tag,{dto.FormattedTimestamp},{dto.DialogId}");
-        searchTagCsvData.AppendLine($"{searchTagId2},gibberish,{dto.FormattedTimestamp},{dto.DialogId}");
-        searchTagCsvData.AppendLine($"{searchTagId3},Should we randomize this?,{dto.FormattedTimestamp},{dto.DialogId}");
+        var rng = new Random(dto.DialogId.ToString().GetHashCode());
+
+        var tagText1 = Words.Norwegian.Length != 0
+            ? Words.Norwegian[rng.Next(0, Words.Norwegian.Length)]
+            : $"Norsk {Guid.NewGuid().ToString()[..8]}";
+
+        var tagText2 = Words.Norwegian.Length != 0
+            ? Words.Norwegian[rng.Next(0, Words.Norwegian.Length)]
+            : $"Norsk {Guid.NewGuid().ToString()[..8]}";
+
+        var tagText3 = Words.English.Length != 0
+            ? Words.English[rng.Next(0, Words.English.Length)]
+            : $"English {Guid.NewGuid().ToString()[..8]}";
+
+        searchTagCsvData.AppendLine($"{searchTagId1},{tagText1},{dto.FormattedTimestamp},{dto.DialogId}");
+        searchTagCsvData.AppendLine($"{searchTagId2},{tagText2},{dto.FormattedTimestamp},{dto.DialogId}");
+        searchTagCsvData.AppendLine($"{searchTagId3},{tagText3},{dto.FormattedTimestamp},{dto.DialogId}");
 
         return searchTagCsvData.ToString();
     }

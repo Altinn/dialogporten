28143655f18b1f9f319cfe7f62bc2e12beafb854
Magnus Sandgren <5285192+MagnusSandgren@users.noreply.github.com>
2025-02-21 13:50:06 +0100

    chore: Return claimsprinciple on dialogtoken validate (#1933)

--- src/Digdir.Library.Dialogporten.WebApiClient/Services/DefaultValidationResult.cs
+++ src/Digdir.Library.Dialogporten.WebApiClient/Services/DefaultValidationResult.cs
@@ -1,9 +1,14 @@
+using System.Diagnostics.CodeAnalysis;
+using System.Security.Claims;
+
 namespace Altinn.ApiClients.Dialogporten.Services;
 
 internal sealed class DefaultValidationResult : IValidationResult
 {
     public Dictionary<string, List<string>> Errors { get; } = [];
+    [MemberNotNullWhen(true, nameof(ClaimsPrincipal))]
     public bool IsValid => Errors.Count == 0;
+    public ClaimsPrincipal? ClaimsPrincipal { get; internal set; }
 
     internal void AddError(string key, string message)
     {

74d19d9f9acfbe5f60c377aa9067253fec6b5108
Ole Jørgen Skogstad <skogstad@softis.net>
2025-02-24 17:51:15 +0100

    --wip-- [skip ci]

---
+++ tests/Digdir.Domain.Dialogporten.WebApiClient.E2E.Tests/ConditionalSkipFactAttribute.cs
@@ -0,0 +1,17 @@
+using Xunit;
+
+namespace Digdir.Domain.Dialogporten.WebApiClient.E2E.Tests;
+
+internal sealed class ConditionalSkipFactAttribute : FactAttribute
+{
+    private const string RunE2E = "RunE2E";
+
+    public ConditionalSkipFactAttribute()
+    {
+        var disableSkippingE2E = Environment.GetEnvironmentVariable(RunE2E);
+        if (disableSkippingE2E != "true")
+        {
+            Skip = $"To run E2E tests, set the environment variable {RunE2E} to 'true'";
+        }
+    }
+}

a7727611b9f36374760bfad561ee3f84e8b824c9
Amund Myrbostad <Lullinj98@gmail.com>
2025-02-27 10:58:59 +0100

    Clean up

--- src/Digdir.Domain.Dialogporten.Domain/Actors/ActorName.cs
+++ src/Digdir.Domain.Dialogporten.Domain/Actors/ActorName.cs
@@ -6,7 +6,7 @@ namespace Digdir.Domain.Dialogporten.Domain.Actors;
 public sealed class ActorName : IImmutableEntity
 {
     public Guid Id { get; set; }
-    public string? ActorId { get; set; } = null!;
+    public string? ActorId { get; set; }
     public string Name { get; set; } = null!;
     public DateTimeOffset CreatedAt { get; set; }
 }

f9a3a298ab0a01c2536dc0532bf0c21970f8ea73
Ole Jørgen Skogstad <skogstad@softis.net>
2025-04-10 13:41:54 +0200

    new migration

---
+++ src/Digdir.Domain.Dialogporten.Infrastructure/Persistence/Migrations/20250410114044_AddApiDialog.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Digdir.Domain.Dialogporten.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddApiDialog : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "ApiDialog",
+                table: "Dialog",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Dialog_ApiDialog",
+                table: "Dialog",
+                column: "ApiDialog");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Dialog_ApiDialog",
+                table: "Dialog");
+
+            migrationBuilder.DropColumn(
+                name: "ApiDialog",
+                table: "Dialog");
+        }
+    }
+}

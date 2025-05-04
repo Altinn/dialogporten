cebe5616205798db232b345d5578258882bad12c
Bjørn Dybvik Langfors <bdl@digdir.no>
2025-05-04 14:10:26 +0200

    Reduce isolation level for outbox

--- src/Digdir.Domain.Dialogporten.Infrastructure/InfrastructureExtensions.cs
+++ src/Digdir.Domain.Dialogporten.Infrastructure/InfrastructureExtensions.cs
@@ -1,4 +1,5 @@
-﻿using Altinn.ApiClients.Maskinporten.Extensions;
+﻿using System.Data;
+using Altinn.ApiClients.Maskinporten.Extensions;
 using Altinn.ApiClients.Maskinporten.Interfaces;
 using Altinn.ApiClients.Maskinporten.Services;
 using Digdir.Domain.Dialogporten.Application.Externals;
@@ -195,6 +196,12 @@ public static class InfrastructureExtensions
             {
                 o.UsePostgres();
                 o.UseBusOutbox();
+
+                // This lowers the isolation level from Serializable to ReadCommitted. This avoids contention issues
+                // and provides sufficient guarantees for the outbox bus using polling with FOR UPDATE SKIP LOCKED,
+                // where MassTransit only perform message forwarding to Azure Service Bus and isn't performing any
+                // mutation of the outbox besides deleting rows after successful passing to ASB.
+                o.IsolationLevel = IsolationLevel.ReadCommitted;
             });
 
             foreach (var customConfiguration in customConfigurations)

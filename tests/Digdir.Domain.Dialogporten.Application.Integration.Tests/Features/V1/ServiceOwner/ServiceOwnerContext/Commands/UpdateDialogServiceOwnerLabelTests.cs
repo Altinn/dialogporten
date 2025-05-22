8884f76cbda73eed50645695dcd0a9223922541a
Ole Jørgen Skogstad <skogstad@softis.net>
2025-05-22 10:21:47 +0200

    updt tests

--- tests/Digdir.Domain.Dialogporten.Application.Integration.Tests/Features/V1/ServiceOwner/ServiceOwnerContext/Commands/UpdateDialogServiceOwnerLabelTests.cs
+++ tests/Digdir.Domain.Dialogporten.Application.Integration.Tests/Features/V1/ServiceOwner/ServiceOwnerContext/Commands/UpdateDialogServiceOwnerLabelTests.cs
@@ -1,6 +1,7 @@
 using Digdir.Domain.Dialogporten.Application.Features.V1.ServiceOwner.Dialogs.Queries.Get;
 using Digdir.Domain.Dialogporten.Application.Features.V1.ServiceOwner.ServiceOwnerContext.Commands.Update;
 using Digdir.Domain.Dialogporten.Application.Integration.Tests.Common;
+using Digdir.Domain.Dialogporten.Domain.Common;
 using Digdir.Domain.Dialogporten.Domain.DialogServiceOwnerContexts.Entities;
 using Digdir.Library.Entity.Abstractions.Features.Identifiable;
 using Digdir.Tool.Dialogporten.GenerateFakeData;
@@ -125,139 +126,104 @@ public class UpdateDialogServiceOwnerLabelTests : ApplicationCollectionFixture
                 .Contains("duplicate"));
     }
 
-    //
-    // [Fact]
-    // public async Task Cannot_SetServiceOwnerLabels_With_Invalid_Length()
-    // {
-    //     // Arrange
-    //     var dialogId = IdentifiableExtensions.CreateVersion7();
-    //     var createDialogCommand = DialogGenerator.GenerateSimpleFakeCreateDialogCommand(id: dialogId);
-    //     await Application.Send(createDialogCommand);
-    //
-    //     var setServiceOwnerLabelsCommand = new SetServiceOwnerLabelsCommand
-    //     {
-    //         DialogId = dialogId,
-    //         ServiceOwnerLabels =
-    //         [
-    //             new() { Value = new string('a', Constants.MinSearchStringLength - 1) },
-    //             new() { Value = new string('a', Constants.DefaultMaxStringLength + 1) }
-    //         ]
-    //     };
-    //
-    //     // Act
-    //     var response = await Application.Send(setServiceOwnerLabelsCommand);
-    //
-    //     // Assert
-    //     response.TryPickT3(out var validationError, out _).Should().BeTrue();
-    //     validationError.Errors
-    //         .Should()
-    //         .ContainSingle(x => x.ErrorMessage
-    //             .Contains("at least"));
-    //
-    //     validationError.Errors
-    //         .Should()
-    //         .ContainSingle(x => x.ErrorMessage
-    //             .Contains("or fewer"));
-    // }
-    //
-    // [Fact]
-    // public async Task SetServiceOwnerLabels_Should_Update_ServiceOwnerContext_Revision()
-    // {
-    //     // Arrange
-    //     var dialogId = IdentifiableExtensions.CreateVersion7();
-    //     var createDialogCommand = DialogGenerator.GenerateSimpleFakeCreateDialogCommand(id: dialogId);
-    //     await Application.Send(createDialogCommand);
-    //
-    //     var originalServiceOwnerContextRevision =
-    //         (await Application.GetDbEntities<Domain.ServiceOwnerContexts.Entities.ServiceOwnerContext>())
-    //         .Single(x => x.DialogId == dialogId).Revision;
-    //
-    //     var setServiceOwnerLabelsCommand = new SetServiceOwnerLabelsCommand
-    //     {
-    //         DialogId = dialogId,
-    //         ServiceOwnerLabels =
-    //         [
-    //             new() { Value = "Scadrial" }
-    //         ]
-    //     };
-    //
-    //     var labelCount = setServiceOwnerLabelsCommand.ServiceOwnerLabels.Count;
-    //
-    //     // Act
-    //     var response = await Application.Send(setServiceOwnerLabelsCommand);
-    //
-    //     // Assert
-    //     response.TryPickT0(out var success, out _).Should().BeTrue();
-    //
-    //     originalServiceOwnerContextRevision.Should().NotBe(Guid.Empty);
-    //     success.Revision.Should().NotBe(originalServiceOwnerContextRevision);
-    //
-    //     await Application.AssertEntityCountAsync<ServiceOwnerLabel>(count: labelCount);
-    // }
-    //
-    // [Fact]
-    // public async Task SetServiceOwnerLabels_Should_Not_Update_Dialog_Revision()
-    // {
-    //     // Arrange
-    //     var dialogId = IdentifiableExtensions.CreateVersion7();
-    //     var createDialogCommand = DialogGenerator.GenerateSimpleFakeCreateDialogCommand(id: dialogId);
-    //     await Application.Send(createDialogCommand);
-    //
-    //     var originalDialogRevision = (await Application.GetDbEntities<DialogEntity>())
-    //         .Single(x => x.Id == dialogId).Revision;
-    //
-    //     var setServiceOwnerLabelsCommand = new SetServiceOwnerLabelsCommand
-    //     {
-    //         DialogId = dialogId,
-    //         ServiceOwnerLabels =
-    //         [
-    //             new() { Value = "Scadrial" }
-    //         ]
-    //     };
-    //
-    //     var labelCount = setServiceOwnerLabelsCommand.ServiceOwnerLabels.Count;
-    //
-    //     // Act
-    //     var response = await Application.Send(setServiceOwnerLabelsCommand);
-    //
-    //     // Assert
-    //     response.TryPickT0(out _, out _).Should().BeTrue();
-    //     var dialogRevisionAfterUpdate = (await Application.GetDbEntities<DialogEntity>())
-    //         .Single(x => x.Id == dialogId).Revision;
-    //
-    //     originalDialogRevision.Should().NotBe(Guid.Empty);
-    //     dialogRevisionAfterUpdate.Should().Be(originalDialogRevision);
-    //
-    //     await Application.AssertEntityCountAsync<ServiceOwnerLabel>(count: labelCount);
-    // }
-    //
-    // [Fact]
-    // public async Task Cannot_Create_More_Than_Max_Allowed_ServiceOwner_Labels()
-    // {
-    //     // Arrange
-    //     var dialogId = IdentifiableExtensions.CreateVersion7();
-    //     var createDialogCommand = DialogGenerator.GenerateSimpleFakeCreateDialogCommand(id: dialogId);
-    //     await Application.Send(createDialogCommand);
-    //
-    //     var setServiceOwnerLabelsCommand = new SetServiceOwnerLabelsCommand
-    //     {
-    //         DialogId = dialogId,
-    //         ServiceOwnerLabels = []
-    //     };
-    //
-    //     Enumerable.Range(0, ServiceOwnerLabel.MaxNumberOfLabels + 1).ToList()
-    //         .ForEach(i => setServiceOwnerLabelsCommand
-    //             .ServiceOwnerLabels.Add(new() { Value = $"label{i}" }));
-    //
-    //     // Act
-    //     var response = await Application.Send(setServiceOwnerLabelsCommand);
-    //
-    //     // Assert
-    //     response.TryPickT3(out var validationError, out _).Should().BeTrue();
-    //     validationError.Errors
-    //         .Should()
-    //         .ContainSingle(x => x.ErrorMessage
-    //             .Contains("Maximum") && x.ErrorMessage
-    //             .Contains($"{ServiceOwnerLabel.MaxNumberOfLabels}"));
-    // }
+    [Fact]
+    public async Task Cannot_Update_ServiceOwnerLabels_With_Invalid_Length()
+    {
+        // Arrange
+        var dialogId = IdentifiableExtensions.CreateVersion7();
+        var createDialogCommand = DialogGenerator.GenerateSimpleFakeCreateDialogCommand(id: dialogId);
+        await Application.Send(createDialogCommand);
+
+        var setServiceOwnerLabelsCommand = new UpdateDialogServiceOwnerContextCommand
+        {
+            DialogId = dialogId,
+            Dto = new()
+            {
+                ServiceOwnerLabels =
+                [
+                    new() { Value = new string('a', Constants.MinSearchStringLength - 1) },
+                    new() { Value = new string('a', Constants.DefaultMaxStringLength + 1) }
+                ]
+            }
+        };
+
+        // Act
+        var response = await Application.Send(setServiceOwnerLabelsCommand);
+
+        // Assert
+        response.TryPickT1(out var validationError, out _).Should().BeTrue();
+        validationError.Errors
+            .Should()
+            .ContainSingle(x => x.ErrorMessage
+                .Contains("at least"));
+
+        validationError.Errors
+            .Should()
+            .ContainSingle(x => x.ErrorMessage
+                .Contains("or fewer"));
+    }
+
+    [Fact]
+    public async Task Update_ServiceOwnerLabels_Should_Update_ServiceOwnerContext_Revision()
+    {
+        // Arrange
+        var dialogId = IdentifiableExtensions.CreateVersion7();
+        var createDialogCommand = DialogGenerator.GenerateSimpleFakeCreateDialogCommand(id: dialogId);
+        await Application.Send(createDialogCommand);
+
+        var originalServiceOwnerContextRevision =
+            (await Application.GetDbEntities<DialogServiceOwnerContext>())
+            .Single(x => x.DialogId == dialogId).Revision;
+
+        var setServiceOwnerLabelsCommand = new UpdateDialogServiceOwnerContextCommand
+        {
+            DialogId = dialogId,
+            Dto = new()
+            {
+                ServiceOwnerLabels =
+                [
+                    new() { Value = "Scadrial" }
+                ]
+            }
+        };
+
+        // Act
+        var response = await Application.Send(setServiceOwnerLabelsCommand);
+
+        // Assert
+        response.TryPickT0(out var success, out _).Should().BeTrue();
+
+        originalServiceOwnerContextRevision.Should().NotBe(Guid.Empty);
+        success.Revision.Should().NotBe(originalServiceOwnerContextRevision);
+    }
+
+    [Fact]
+    public async Task Cannot_Update_With_More_Than_Max_Allowed_ServiceOwner_Labels()
+    {
+        // Arrange
+        var dialogId = IdentifiableExtensions.CreateVersion7();
+        var createDialogCommand = DialogGenerator.GenerateSimpleFakeCreateDialogCommand(id: dialogId);
+        await Application.Send(createDialogCommand);
+
+        var setServiceOwnerLabelsCommand = new UpdateDialogServiceOwnerContextCommand
+        {
+            DialogId = dialogId,
+            Dto = new()
+        };
+
+        Enumerable.Range(0, DialogServiceOwnerLabel.MaxNumberOfLabels + 1).ToList()
+            .ForEach(i => setServiceOwnerLabelsCommand
+                .Dto.ServiceOwnerLabels.Add(new() { Value = $"label{i}" }));
+
+        // Act
+        var response = await Application.Send(setServiceOwnerLabelsCommand);
+
+        // Assert
+        response.TryPickT1(out var validationError, out _).Should().BeTrue();
+        validationError.Errors
+            .Should()
+            .ContainSingle(x => x.ErrorMessage
+                .Contains("Maximum") && x.ErrorMessage
+                .Contains($"{DialogServiceOwnerLabel.MaxNumberOfLabels}"));
+    }
 }

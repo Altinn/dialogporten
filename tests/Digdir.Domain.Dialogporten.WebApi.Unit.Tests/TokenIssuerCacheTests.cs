27ba26c835ac9b5834cc7fc7c7149d424f26b7d1
Bjørn Dybvik Langfors <bjorn@langfors.no>
2025-05-22 12:58:55 +0200

    Fix TokenIssuerCache semaphore release and add concurrency test

---
+++ tests/Digdir.Domain.Dialogporten.WebApi.Unit.Tests/TokenIssuerCacheTests.cs
@@ -0,0 +1,37 @@
+using System.Threading.Tasks;
+using Digdir.Domain.Dialogporten.WebApi.Common;
+using Digdir.Domain.Dialogporten.WebApi.Common.Authentication;
+using Microsoft.Extensions.Options;
+using Xunit;
+
+namespace Digdir.Domain.Dialogporten.WebApi.Unit.Tests;
+
+public class TokenIssuerCacheTests
+{
+    [Fact]
+    public async Task EnsureInitializedAsync_Should_Not_Deadlock_When_Called_Concurrently()
+    {
+        // Arrange
+        var settings = new WebApiSettings
+        {
+            Authentication = new AuthenticationOptions
+            {
+                JwtBearerTokenSchemas = new List<JwtBearerTokenSchemasOptions>()
+            }
+        };
+        var cache = new TokenIssuerCache(Options.Create(settings));
+
+        var tasks = Enumerable.Range(0, 10)
+            .Select(_ => cache.GetIssuerForScheme("test"));
+
+        // Act
+        var allTasks = Task.WhenAll(tasks);
+        var completed = await Task.WhenAny(allTasks, Task.Delay(TimeSpan.FromSeconds(1)));
+
+        // Assert
+        Assert.Same(allTasks, completed);
+        await allTasks; // propagate exceptions if any
+
+        cache.Dispose();
+    }
+}

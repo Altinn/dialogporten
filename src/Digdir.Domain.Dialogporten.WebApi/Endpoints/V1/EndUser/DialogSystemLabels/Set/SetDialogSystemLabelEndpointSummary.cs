d8ec0dcc24fcb235712f96693d9a3612e7a6ba99
Bjørn Dybvik Langfors <bjorn@langfors.no>
2025-05-23 12:23:41 +0200

    docs(api): add summaries for system label endpoints

---
+++ src/Digdir.Domain.Dialogporten.WebApi/Endpoints/V1/EndUser/DialogSystemLabels/Set/SetDialogSystemLabelEndpointSummary.cs
@@ -0,0 +1,29 @@
+using Digdir.Domain.Dialogporten.WebApi.Common;
+using Digdir.Domain.Dialogporten.WebApi.Common.Extensions;
+using Digdir.Domain.Dialogporten.WebApi.Endpoints.V1.Common.Headers;
+using FastEndpoints;
+
+namespace Digdir.Domain.Dialogporten.WebApi.Endpoints.V1.EndUser.DialogSystemLabels.Set;
+
+public sealed class SetDialogSystemLabelEndpointSummary : Summary<SetDialogSystemLabelEndpoint>
+{
+    public SetDialogSystemLabelEndpointSummary()
+    {
+        Summary = "Sets the system label of a dialog";
+        Description = $"""
+                       Sets the system label of the dialog.
+
+                       {Constants.SwaggerSummary.OptimisticConcurrencyNote}
+                       """;
+
+        ResponseHeaders = [HttpResponseHeaderExamples.NewDialogETagHeader(StatusCodes.Status204NoContent)];
+        Responses[StatusCodes.Status204NoContent] = Constants.SwaggerSummary.Updated.FormatInvariant("system label");
+        Responses[StatusCodes.Status400BadRequest] = Constants.SwaggerSummary.ValidationError;
+        Responses[StatusCodes.Status401Unauthorized] = Constants.SwaggerSummary.EndUserAuthenticationFailure;
+        Responses[StatusCodes.Status403Forbidden] = Constants.SwaggerSummary.AccessDeniedToDialog.FormatInvariant("update");
+        Responses[StatusCodes.Status404NotFound] = Constants.SwaggerSummary.DialogNotFound;
+        Responses[StatusCodes.Status410Gone] = Constants.SwaggerSummary.DialogDeleted;
+        Responses[StatusCodes.Status412PreconditionFailed] = Constants.SwaggerSummary.RevisionMismatch;
+        Responses[StatusCodes.Status422UnprocessableEntity] = Constants.SwaggerSummary.DomainError;
+    }
+}

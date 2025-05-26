439722d1cf176ad1b91f8d41ef3fe0d42c5917ac
Bjørn Dybvik Langfors <bdl@digdir.no>
2025-05-26 13:26:29 +0200

    test(e2e): update label setting tests (#2360)
    ## Summary
    - update k6 enduser label tests to new `systemLabels` array
    - cover invalid multi-label scenario
    - add serviceowner label e2e test and wire into suite
    
    ## Testing
    - `dotnet build Digdir.Domain.Dialogporten.sln -c Release`
    - `dotnet test Digdir.Domain.Dialogporten.sln --no-build --filter
    'FullyQualifiedName!~Integration' -c Release`

--- tests/k6/tests/serviceowner/all-tests.js
+++ tests/k6/tests/serviceowner/all-tests.js
@@ -14,6 +14,7 @@ import { default as dialogDetails } from './dialogDetails.js';
 import { default as dialogRestore } from './dialogRestore.js';
 import { default as dialogSearch } from './dialogSearch.js';
 import { default as dialogUpdateActivity } from './dialogUpdateActivity.js';
+import { default as dialogSystemLabels } from './dialogSystemLabels.js';
 
 export default function() {
   authentication();
@@ -31,4 +32,5 @@ export default function() {
   dialogRestore();
   dialogSearch();
   dialogUpdateActivity();
+  dialogSystemLabels();
 }

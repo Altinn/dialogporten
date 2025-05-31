6f5e5fa7defcfbac5c8f91023b34f14c4e4b341b
Ole Jørgen Skogstad <skogstad@softis.net>
2025-05-31 15:14:18 +0200

    eu deleted assert correct type

--- tests/Digdir.Domain.Dialogporten.Application.Integration.Tests/Features/V1/EndUser/Dialogs/Queries/DeletedDialogTests.cs
+++ tests/Digdir.Domain.Dialogporten.Application.Integration.Tests/Features/V1/EndUser/Dialogs/Queries/DeletedDialogTests.cs
@@ -1,6 +1,7 @@
 using Digdir.Domain.Dialogporten.Application.Common.ReturnTypes;
 using Digdir.Domain.Dialogporten.Application.Integration.Tests.Common;
 using Digdir.Domain.Dialogporten.Application.Integration.Tests.Common.ApplicationFlow;
+using Digdir.Domain.Dialogporten.Domain.Dialogs.Entities;
 using FluentAssertions;
 
 namespace Digdir.Domain.Dialogporten.Application.Integration.Tests.Features.V1.EndUser.Dialogs.Queries;
@@ -9,18 +10,11 @@ namespace Digdir.Domain.Dialogporten.Application.Integration.Tests.Features.V1.E
 public class DeletedDialogTests(DialogApplication application) : ApplicationCollectionFixture(application)
 {
     [Fact]
-    public async Task Fetching_Deleted_Dialog_Should_Return_Gone()
-    {
-        // Using FlowBuilder
-        await FlowBuilder.For(Application)
+    public Task Fetching_Deleted_Dialog_Should_Return_Gone() =>
+        FlowBuilder.For(Application)
             .CreateSimpleDialog()
             .DeleteDialog()
             .GetEndUserDialog()
-            .ExecuteAndAssert<EntityDeleted>(entityDeleted =>
-            {
-                entityDeleted.Should().NotBeNull();
-                entityDeleted.Message.Should().Contain("is removed");
-            });
-
-    }
+            .ExecuteAndAssert<EntityDeleted<DialogEntity>>(entityDeleted =>
+                entityDeleted.Message.Should().Contain("is removed"));
 }

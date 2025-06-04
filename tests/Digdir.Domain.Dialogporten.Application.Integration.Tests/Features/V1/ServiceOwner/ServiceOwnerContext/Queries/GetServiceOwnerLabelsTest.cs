a358effe5b97fcfb4cc923d9903edef913438270
Ole Jørgen Skogstad <skogstad@softis.net>
2025-06-04 15:12:34 +0200

    get labels test

--- tests/Digdir.Domain.Dialogporten.Application.Integration.Tests/Features/V1/ServiceOwner/ServiceOwnerContext/Queries/GetServiceOwnerLabelsTest.cs
+++ tests/Digdir.Domain.Dialogporten.Application.Integration.Tests/Features/V1/ServiceOwner/ServiceOwnerContext/Queries/GetServiceOwnerLabelsTest.cs
@@ -1,8 +1,10 @@
+using Digdir.Domain.Dialogporten.Application.Common.ReturnTypes;
 using Digdir.Domain.Dialogporten.Application.Features.V1.ServiceOwner.ServiceOwnerContext.ServiceOwnerLabels.Queries.Get;
 using Digdir.Domain.Dialogporten.Application.Integration.Tests.Common;
-using Digdir.Library.Entity.Abstractions.Features.Identifiable;
-using Digdir.Tool.Dialogporten.GenerateFakeData;
+using Digdir.Domain.Dialogporten.Application.Integration.Tests.Common.ApplicationFlow;
+using Digdir.Domain.Dialogporten.Domain.Dialogs.Entities;
 using FluentAssertions;
+using static Digdir.Domain.Dialogporten.Application.Integration.Tests.Common.Common;
 using ServiceOwnerLabelDto = Digdir.Domain.Dialogporten.Application.Features.V1.ServiceOwner.Dialogs.Commands.Create.ServiceOwnerLabelDto;
 
 namespace Digdir.Domain.Dialogporten.Application.Integration.Tests.Features.V1.ServiceOwner.ServiceOwnerContext.Queries;
@@ -13,47 +15,18 @@ public class GetServiceOwnerLabelsTest : ApplicationCollectionFixture
     public GetServiceOwnerLabelsTest(DialogApplication application) : base(application) { }
 
     [Fact]
-    public async Task Can_Get_ServiceOwnerLabels()
-    {
-        // Arrange
-        var dialogId = IdentifiableExtensions.CreateVersion7();
-        var createDialogCommand = DialogGenerator.GenerateSimpleFakeCreateDialogCommand(id: dialogId);
-        var labels = CreateLabels("Scadrial", "Roshar", "Sel");
-        createDialogCommand.Dto.ServiceOwnerContext!.ServiceOwnerLabels = labels;
-
-        await Application.Send(createDialogCommand);
-
-        // Act
-        var getServiceOwnerLabelsQuery = new GetServiceOwnerLabelsQuery
-        {
-            DialogId = dialogId
-        };
-
-        // Act
-        var response = await Application.Send(getServiceOwnerLabelsQuery);
-        response.TryPickT0(out var serviceOwnerLabels, out _).Should().BeTrue();
-        serviceOwnerLabels.Should().NotBeNull();
-        serviceOwnerLabels.Labels.Should().HaveCount(labels.Count);
-    }
+    public Task Can_Get_ServiceOwnerLabels() =>
+        FlowBuilder.For(Application)
+            .CreateSimpleDialog(x => x.Dto.ServiceOwnerContext!.ServiceOwnerLabels
+                = CreateLabels("Scadrial", "Roshar", "Sel"))
+            .SendCommand((_, ctx) => new GetServiceOwnerLabelsQuery { DialogId = ctx.GetDialogId() })
+            .ExecuteAndAssert<ServiceOwnerLabelResultDto>(x => x.Labels.Should().HaveCount(3));
 
     [Fact]
-    public async Task Get_ServiceOwnerLabels_With_Invalid_DialogId_Returns_NotFound()
-    {
-        // Arrange
-        var dialogId = IdentifiableExtensions.CreateVersion7();
-        var getServiceOwnerLabelsQuery = new GetServiceOwnerLabelsQuery
-        {
-            DialogId = dialogId
-        };
-
-        // Act
-        var response = await Application.Send(getServiceOwnerLabelsQuery);
-
-        // Assert
-        response.TryPickT1(out var notFoundError, out _).Should().Be(true);
-        notFoundError.Should().NotBeNull();
-        notFoundError.Message.Should().Contain(dialogId.ToString());
-    }
+    public Task Get_ServiceOwnerLabels_With_Invalid_DialogId_Returns_NotFound() =>
+        FlowBuilder.For(Application)
+            .SendCommand(new GetServiceOwnerLabelsQuery { DialogId = NewUuidV7() })
+            .ExecuteAndAssert<EntityNotFound<DialogEntity>>();
 
     private static List<ServiceOwnerLabelDto> CreateLabels(params string[] labels) =>
         labels.Select(label => new ServiceOwnerLabelDto { Value = label }).ToList();

d09d65010d8a7a91b9f2e2e8ec35ecd314f352bc
Ole Jørgen Skogstad <skogstad@softis.net>
2025-06-13 11:22:20 +0200

    trivial: Add authentication level 4 to development users (#2428)

--- src/Digdir.Domain.Dialogporten.WebApi/Common/LocalDevelopmentUser.cs
+++ src/Digdir.Domain.Dialogporten.WebApi/Common/LocalDevelopmentUser.cs
@@ -1,6 +1,7 @@
 ﻿using Digdir.Domain.Dialogporten.Application.Externals.Presentation;
 using System.Security.Claims;
 using Digdir.Domain.Dialogporten.Application.Common.Authorization;
+using AuthConstants = Digdir.Domain.Dialogporten.Application.Common.Authorization.Constants;
 
 namespace Digdir.Domain.Dialogporten.WebApi.Common;
 
@@ -9,6 +10,7 @@ internal sealed class LocalDevelopmentUser : IUser
     private readonly ClaimsPrincipal _principal = new(new ClaimsIdentity(
     [
         new Claim(ClaimTypes.Name, "Local Development User"),
+        new Claim("acr", AuthConstants.IdportenLoaHigh),
         new Claim(ClaimTypes.NameIdentifier, "local-development-user"),
         new Claim("pid", "03886595947"),
         new Claim("scope", string.Join(" ", AuthorizationScope.AllScopes.Value)),

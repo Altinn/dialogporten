57b9955cabe3e46380a34a2fbb925d05b4503766
Amund Myrbostad <Lullinj98@gmail.com>
2025-06-16 09:43:11 +0200

    Fixed logic mistake

--- src/Digdir.Domain.Dialogporten.Application/Features/V1/Common/DialogUnopenedContent.cs
+++ src/Digdir.Domain.Dialogporten.Application/Features/V1/Common/DialogUnopenedContent.cs
@@ -2,6 +2,7 @@ using Digdir.Domain.Dialogporten.Application.Externals;
 using Digdir.Domain.Dialogporten.Domain.Dialogs.Entities;
 using Digdir.Domain.Dialogporten.Domain.Dialogs.Entities.Activities;
 using Digdir.Domain.Dialogporten.Domain.Dialogs.Entities.Transmissions;
+using Constants = Digdir.Domain.Dialogporten.Application.Common.ResourceRegistry.Constants;
 
 namespace Digdir.Domain.Dialogporten.Application.Features.V1.Common;
 
@@ -10,21 +11,19 @@ public static class DialogUnopenedContent
     public static bool HasUnopenedContent(DialogEntity dialog, ServiceResourceInformation? serviceResourceInformation)
     {
 
-        // Checks if transmissions of all types except Correction or Acceptance have any activities with TransmissionOpened
+        // Checks if transmissions of all types except Correction or Submission have any activities with TransmissionOpened
         var hasUnopenedContent = dialog.Transmissions
                                        .Where(transmission => transmission.TypeId is not
-                                           (DialogTransmissionType.Values.Correction or DialogTransmissionType.Values.Acceptance))
+                                           (DialogTransmissionType.Values.Correction or DialogTransmissionType.Values.Submission))
                                        .SelectMany(transmission => transmission.Activities)
-                                       .All(activity => activity.TypeId != DialogActivityType.Values.TransmissionOpened);
+                                       .Any(activity => activity.TypeId != DialogActivityType.Values.TransmissionOpened);
 
 
-        if (hasUnopenedContent) return true;
-
-        if (serviceResourceInformation?.ResourceType == "CorrespondenceService")
+        if (serviceResourceInformation?.ResourceType == Constants.CorrespondenceService)
         {
-            return dialog.Activities.All(x => x.TypeId != DialogActivityType.Values.CorrespondenceOpened);
+            hasUnopenedContent = dialog.Activities.Any(x => x.TypeId != DialogActivityType.Values.CorrespondenceOpened);
         }
 
-        return false;
+        return hasUnopenedContent;
     }
 }

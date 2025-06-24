e988fd9ef42567321ed8f7422888bff96e21e429
Amund Myrbostad <Lullinj98@gmail.com>
2025-06-24 12:41:54 +0200

    Created more tests

--- tests/Digdir.Domain.Dialogporten.Application.Integration.Tests/Features/V1/ServiceOwner/Activities/BumpFormSavedAtTests.cs
+++ tests/Digdir.Domain.Dialogporten.Application.Integration.Tests/Features/V1/ServiceOwner/Activities/BumpFormSavedAtTests.cs
@@ -1,3 +1,4 @@
+using Digdir.Domain.Dialogporten.Application.Common.ReturnTypes;
 using Digdir.Domain.Dialogporten.Application.Features.V1.Common.Localizations;
 using Digdir.Domain.Dialogporten.Application.Features.V1.ServiceOwner.Common.Actors;
 using Digdir.Domain.Dialogporten.Application.Features.V1.ServiceOwner.Dialogs.Commands.BumpFormSaved;
@@ -16,7 +17,7 @@ namespace Digdir.Domain.Dialogporten.Application.Integration.Tests.Features.V1.S
 public class BumpFormSavedAtTests(DialogApplication application) : ApplicationCollectionFixture(application)
 {
     [Fact]
-    public async Task BumpFormSavedAt()
+    public async Task Can_Bump_FormSavedAt_When_Dialog_Id_Draft()
     {
         var timestamp = DateTimeOffset.UtcNow - TimeSpan.FromDays(1);
         await FlowBuilder.For(Application)
@@ -54,20 +55,39 @@ public class BumpFormSavedAtTests(DialogApplication application) : ApplicationCo
                     }
                 ];
                 x.Dto.CreatedAt = timestamp;
-            }).AssertResult<CreateDialogSuccess>()
-            .SendCommand(ctx =>
-            {
-                var command = new BumpFormSavedCommand
-                {
-                    DialogId = ctx.GetDialogId()
-                };
-                return command;
             })
-            .AssertResult<BumpFormSavedSuccess>()
-            .SendCommand((_, ctx) => new GetDialogQuery
+            .AssertResult<CreateDialogSuccess>()
+            .SendCommand(ctx => new BumpFormSavedCommand
             {
                 DialogId = ctx.GetDialogId()
             })
             .ExecuteAndAssert<DialogDto>(x => x.Activities.Last().CreatedAt.Should().BeMoreThan(timestamp.Offset));
+    }
+
+    [Fact]
+    public async Task Cannot_Bump_FormSavedAt_When_FormSaved_Is_Not_Latest_Activity()
+    {
+        await FlowBuilder.For(Application)
+            .CreateSimpleDialog(x => x.Dto.Status = DialogStatus.Values.Draft)
+            .AssertResult<CreateDialogSuccess>()
+            .SendCommand(ctx => new BumpFormSavedCommand
+            {
+                DialogId = ctx.DialogId,
+            })
+            .ExecuteAndAssert<DomainError>(x => x.ShouldHaveErrorWithText("Latest activity is not of type FormSaved"));
+    }
+
+
+    [Fact]
+    public async Task Cannot_Bump_FormSavedAt_When_Dialog_Is_Not_Draft()
+    {
+        await FlowBuilder.For(Application)
+            .CreateSimpleDialog(x => x.Dto.Status = DialogStatus.Values.InProgress)
+            .AssertResult<CreateDialogSuccess>()
+            .SendCommand(ctx => new BumpFormSavedCommand
+            {
+                DialogId = ctx.DialogId,
+            })
+            .ExecuteAndAssert<DomainError>(x => x.ShouldHaveErrorWithText("Can only bump timestamp when dialog status is Draft"));
     }
 }

050593b71326d0fd284bde328741908702d27267
Ole Jørgen Skogstad <skogstad@softis.net>
2025-07-09 14:10:04 +0200

    Do dialogDto as well, add filter param

--- tests/Digdir.Domain.Dialogporten.GraphQl.Unit.Tests/ObjectTypes/PropertyNameComparisonTests.cs
+++ tests/Digdir.Domain.Dialogporten.GraphQl.Unit.Tests/ObjectTypes/PropertyNameComparisonTests.cs
@@ -1,61 +1,91 @@
 using Digdir.Domain.Dialogporten.Application.Features.V1.EndUser.Dialogs.Queries.Get;
 using Digdir.Domain.Dialogporten.GraphQL.EndUser.Common;
 using Digdir.Domain.Dialogporten.GraphQL.EndUser.DialogById;
+using Digdir.Domain.Dialogporten.GraphQL.EndUser.SearchDialogs;
+using DialogDto = Digdir.Domain.Dialogporten.Application.Features.V1.EndUser.Dialogs.Queries.Get.DialogDto;
+using SearchDialogDto = Digdir.Domain.Dialogporten.Application.Features.V1.EndUser.Dialogs.Queries.Search.DialogDto;
+#pragma warning disable CS0618 // Type or member is obsolete
 
 namespace Digdir.Domain.Dialogporten.GraphQl.Unit.Tests.ObjectTypes;
 
 public class PropertyNameComparisonTests
 {
-    private sealed class PropertyNameComparisonTestsData : TheoryData<Type, Type>
+    private sealed class PropertyNameComparisonTestsData : TheoryData<Type, Type, Func<string, bool>?>
     {
         public PropertyNameComparisonTestsData()
         {
-            // DialogDto for detail and search queries have their own tests since they have
-            // properties that are deprecated in the REST API but not present in the GraphQL API.
-            // See DialogDtoTests.cs and DialogSearchDtoTests.cs
-
             Add(typeof(DialogActivityDto),
-                typeof(Activity));
+                typeof(Activity),
+                null);
 
             Add(typeof(DialogEndUserContextDto),
-                typeof(EndUserContext));
+                typeof(EndUserContext),
+                null);
 
             Add(typeof(DialogSeenLogDto),
-                typeof(SeenLog));
+                typeof(SeenLog),
+                null);
 
             Add(typeof(DialogApiActionDto),
-                typeof(ApiAction));
+                typeof(ApiAction),
+                null);
 
             Add(typeof(DialogApiActionEndpointDto),
-                typeof(ApiActionEndpoint));
+                typeof(ApiActionEndpoint),
+                null);
 
             Add(typeof(DialogAttachmentDto),
-                typeof(Attachment));
+                typeof(Attachment),
+                null);
 
             Add(typeof(DialogAttachmentUrlDto),
-                typeof(AttachmentUrl));
+                typeof(AttachmentUrl),
+                null);
 
             Add(typeof(DialogGuiActionDto),
-                typeof(GuiAction));
+                typeof(GuiAction),
+                null);
 
             Add(typeof(DialogTransmissionDto),
-                typeof(Transmission));
+                typeof(Transmission),
+                null);
 
             Add(typeof(DialogTransmissionContentDto),
-                typeof(TransmissionContent));
+                typeof(TransmissionContent),
+                null);
 
             Add(typeof(DialogTransmissionAttachmentDto),
-                typeof(Attachment));
+                typeof(Attachment),
+                null);
 
             Add(typeof(DialogTransmissionAttachmentUrlDto),
-                typeof(AttachmentUrl));
+                typeof(AttachmentUrl),
+                null);
+
+            // Filtering out deprecated properties in the
+            // REST API that are not present in GraphQL
+            Add(typeof(DialogDto),
+                typeof(Dialog),
+                // Delete when dialogDto.SystemLabel is removed
+                name => !name.Contains(nameof(DialogDto.SystemLabel)));
+
+            Add(typeof(SearchDialogDto),
+                typeof(SearchDialog),
+                // Delete when dialogDto.SystemLabel is removed
+                name => !name.Contains(nameof(SearchDialogDto.SystemLabel)));
         }
     }
 
     [Theory, ClassData(typeof(PropertyNameComparisonTestsData))]
-    public void GraphQl_Property_Name_Should_Match_EndUser_Dtos(Type dtoType, Type gqlType)
+    public void GraphQl_Property_Name_Should_Match_EndUser_Dtos(Type dtoType, Type gqlType, Func<string, bool>? filter = null)
     {
-        var dtoProperties = dtoType.GetProperties().Select(p => p.Name).ToList();
+        var dtoProperties = dtoType.GetProperties()
+            .Select(p => p.Name)
+            // Filtering out deprecated properties in the
+            // REST API that are not present in GraphQL
+            .Where(filter ?? (_ => true))
+            .ToList();
+
         var gqlProperties = gqlType.GetProperties().Select(p => p.Name).ToList();
 
         var missingProperties = dtoProperties.Except(gqlProperties, StringComparer.OrdinalIgnoreCase).ToList();

ca36728c9c3a93132e7c35a759e5b94de96d35b3
Ole Jørgen Skogstad <skogstad@softis.net>
2025-07-10 14:54:41 +0200

    fix tag check

--- src/Digdir.Library.Utils.AspNet/PostgresExceptionFilter.cs
+++ src/Digdir.Library.Utils.AspNet/PostgresExceptionFilter.cs
@@ -6,14 +6,20 @@ public class PostgresExceptionFilter : OpenTelemetry.BaseProcessor<Activity>
 {
     public override void OnEnd(Activity activity)
     {
-        if (activity.Kind == ActivityKind.Internal &&
-            activity.Tags.Any(t => t is { Key: "exception.type", Value: "Npgsql.PostgresException" }))
+        if (activity.Tags.ContainsUniqueConstraintError())
         {
-            activity.IsAllDataRequested = false;
+            activity.ActivityTraceFlags &= ~ActivityTraceFlags.Recorded;
         }
         else
         {
             base.OnEnd(activity);
         }
     }
+}
+
+internal static class ActivityExtensions
+{
+    public static bool ContainsUniqueConstraintError(this IEnumerable<KeyValuePair<string, string?>> tags) =>
+        tags.Any(t => t is { Key: "otel.status_code", Value: "ERROR" }) &&
+        tags.Any(t => t.Key == "otel.status_description" && t.Value?.Contains("23505") == true);
 }

a225f11743ec7448e2668cec699977581424cf21
Ole Jørgen Skogstad <skogstad@softis.net>
2025-07-10 15:03:13 +0200

    chore(app): Mute OTEL PostgreSQL exception tracing (#2545)
    <!--- Provide a general summary of your changes in the Title above -->
    
    ## Description
    
    <!--- Describe your changes in detail -->
    
    ## Related Issue(s)
    
    - #2463
    
    ## Verification
    
    - [ ] **Your** code builds clean without any errors or warnings
    - [ ] Manual testing done (required)
    - [ ] Relevant automated test added (if you find this hard, leave it and
    we'll help out)
    
    ## Documentation
    
    - [ ] Documentation is updated (either in `docs`-directory, Altinnpedia
    or a separate linked PR in
    [altinn-studio-docs.](https://github.com/Altinn/altinn-studio-docs), if
    applicable)

---
+++ src/Digdir.Library.Utils.AspNet/PostgresExceptionFilter.cs
@@ -0,0 +1,27 @@
+using System.Diagnostics;
+
+namespace Digdir.Library.Utils.AspNet;
+
+public class PostgresExceptionFilter : OpenTelemetry.BaseProcessor<Activity>
+{
+    public override void OnEnd(Activity activity)
+    {
+        if (activity.Tags.ContainsUniqueConstraintError())
+        {
+            activity.ActivityTraceFlags &= ~ActivityTraceFlags.Recorded;
+        }
+        else
+        {
+            base.OnEnd(activity);
+        }
+    }
+}
+
+internal static class ActivityExtensions
+{
+    public static bool ContainsUniqueConstraintError(this IEnumerable<KeyValuePair<string, string?>> tags) =>
+        tags.Any(t => t is { Key: "otel.status_code", Value: "ERROR" }) &&
+        // "23505" is the PostgreSQL error code for unique constraint violation
+        // This mutes Slack notifications and error logging in AppInsights
+        tags.Any(t => t.Key == "otel.status_description" && t.Value?.Contains("23505") == true);
+}

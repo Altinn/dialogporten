98133d76ae8f660cd45aeb753e68275701998dc4
Ole Jørgen Skogstad <skogstad@softis.net>
2025-07-10 14:59:31 +0200

    last one

--- src/Digdir.Library.Utils.AspNet/PostgresExceptionFilter.cs
+++ src/Digdir.Library.Utils.AspNet/PostgresExceptionFilter.cs
@@ -22,6 +22,6 @@ internal static class ActivityExtensions
     public static bool ContainsUniqueConstraintError(this IEnumerable<KeyValuePair<string, string?>> tags) =>
         tags.Any(t => t is { Key: "otel.status_code", Value: "ERROR" }) &&
         // "23505" is the PostgreSQL error code for unique constraint violation
-        // in order to mute Slack notifications, https://github.com/Altinn/dialogporten/issues/2463
+        // This mutes Slack notifications and error logging in AppInsights
         tags.Any(t => t.Key == "otel.status_description" && t.Value?.Contains("23505") == true);
 }

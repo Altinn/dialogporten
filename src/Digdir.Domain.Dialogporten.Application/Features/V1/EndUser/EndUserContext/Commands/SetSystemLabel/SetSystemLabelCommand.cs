90c8447d44fb120c44f3e574b4fda4af2b0767a4
Ole Jørgen Skogstad <skogstad@softis.net>
2025-07-31 09:10:04 +0200

    test: extensions and tests for markedAsUnopened (#2594)
    ## Related Issue(s)
    
    - #2392

--- src/Digdir.Domain.Dialogporten.Application/Features/V1/EndUser/EndUserContext/Commands/SetSystemLabel/SetSystemLabelCommand.cs
+++ src/Digdir.Domain.Dialogporten.Application/Features/V1/EndUser/EndUserContext/Commands/SetSystemLabel/SetSystemLabelCommand.cs
@@ -14,7 +14,7 @@ public sealed class SetSystemLabelCommand : IRequest<SetSystemLabelResult>
 {
     public Guid DialogId { get; set; }
     public Guid? IfMatchEndUserContextRevision { get; set; }
-    public IReadOnlyCollection<SystemLabel.Values> AddLabels { get; init; } = [];
+    public IReadOnlyCollection<SystemLabel.Values> AddLabels { get; set; } = [];
     public IReadOnlyCollection<SystemLabel.Values> RemoveLabels { get; init; } = [];
 }
 
--- tests/Digdir.Domain.Dialogporten.Application.Integration.Tests/Common/ApplicationFlow/IFlowStepExtensions.cs
+++ tests/Digdir.Domain.Dialogporten.Application.Integration.Tests/Common/ApplicationFlow/IFlowStepExtensions.cs
@@ -23,6 +23,10 @@ using BulkSetSystemLabelResultEU = Digdir.Domain.Dialogporten.Application.Featur
 using BulkSetSystemLabelCommandEU = Digdir.Domain.Dialogporten.Application.Features.V1.EndUser.EndUserContext.Commands.BulkSetSystemLabels.BulkSetSystemLabelCommand;
 using BulkSetSystemLabelResultSO = Digdir.Domain.Dialogporten.Application.Features.V1.ServiceOwner.EndUserContext.Commands.BulkSetSystemLabels.BulkSetSystemLabelResult;
 using BulkSetSystemLabelCommandSO = Digdir.Domain.Dialogporten.Application.Features.V1.ServiceOwner.EndUserContext.Commands.BulkSetSystemLabels.BulkSetSystemLabelCommand;
+using SetSystemLabelResultEU = Digdir.Domain.Dialogporten.Application.Features.V1.EndUser.EndUserContext.Commands.SetSystemLabel.SetSystemLabelResult;
+using SetSystemLabelCommandEU = Digdir.Domain.Dialogporten.Application.Features.V1.EndUser.EndUserContext.Commands.SetSystemLabel.SetSystemLabelCommand;
+using SetSystemLabelResultSO = Digdir.Domain.Dialogporten.Application.Features.V1.ServiceOwner.EndUserContext.Commands.SetSystemLabels.SetSystemLabelResult;
+using SetSystemLabelCommandSO = Digdir.Domain.Dialogporten.Application.Features.V1.ServiceOwner.EndUserContext.Commands.SetSystemLabels.SetSystemLabelCommand;
 
 namespace Digdir.Domain.Dialogporten.Application.Integration.Tests.Common.ApplicationFlow;
 
@@ -86,6 +90,33 @@ public static class IFlowStepExtensions
             initialState.Invoke(command, ctx);
             return command;
         });
+
+    public static IFlowExecutor<SetSystemLabelResultEU> SetSystemLabelsEndUser(this IFlowStep<CreateDialogResult> step,
+        Action<SetSystemLabelCommandEU>? modify = null) =>
+        step.AssertResult<CreateDialogSuccess>()
+            .SendCommand(x =>
+            {
+                var command = new SetSystemLabelCommandEU
+                {
+                    DialogId = x.GetDialogId(),
+                };
+                modify?.Invoke(command);
+                return command;
+            });
+
+    public static IFlowExecutor<SetSystemLabelResultSO> SetSystemLabelsServiceOwner(this IFlowStep<CreateDialogResult> step,
+        Action<SetSystemLabelCommandSO>? modify = null) =>
+        step.AssertResult<CreateDialogSuccess>()
+            .SendCommand(x =>
+            {
+                var command = new SetSystemLabelCommandSO
+                {
+                    DialogId = x.GetDialogId(),
+                    EndUserId = x.GetParty()
+                };
+                modify?.Invoke(command);
+                return command;
+            });
 
     public static IFlowExecutor<PurgeDialogResult> PurgeDialog(this IFlowStep<CreateDialogResult> step,
         Action<PurgeDialogCommand>? modify = null) =>

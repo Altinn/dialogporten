be45f5b1e59fde9bed4120846c2fca70dbc6ff9e
Ole Jørgen Skogstad <skogstad@softis.net>
2025-08-02 13:08:12 +0200

    cmnt

--- tests/Digdir.Domain.Dialogporten.Application.Integration.Tests/Common/AddingEndUserTransmissionSentLabelTestData.cs
+++ tests/Digdir.Domain.Dialogporten.Application.Integration.Tests/Common/AddingEndUserTransmissionSentLabelTestData.cs
@@ -8,6 +8,7 @@ internal sealed class AddingEndUserTransmissionSentLabelTestData : TheoryData<Di
     {
         foreach (var type in Enum.GetValues<DialogTransmissionType.Values>())
         {
+            // (transmissionType, shouldAddSentLabel)
             Add(type, type
                 is DialogTransmissionType.Values.Submission
                 or DialogTransmissionType.Values.Correction);

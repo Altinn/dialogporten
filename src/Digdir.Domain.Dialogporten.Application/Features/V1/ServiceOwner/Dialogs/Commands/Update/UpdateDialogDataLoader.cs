233f850cf2d8c954cf7675f2d7e0936a0fa5a1ca
Amund Myrbostad <Lullinj98@gmail.com>
2025-08-05 11:06:29 +0200

    PR

--- src/Digdir.Domain.Dialogporten.Application/Features/V1/ServiceOwner/Dialogs/Commands/Update/UpdateDialogDataLoader.cs
+++ src/Digdir.Domain.Dialogporten.Application/Features/V1/ServiceOwner/Dialogs/Commands/Update/UpdateDialogDataLoader.cs
@@ -4,16 +4,8 @@ using Digdir.Domain.Dialogporten.Domain.Dialogs.Entities;
 
 namespace Digdir.Domain.Dialogporten.Application.Features.V1.ServiceOwner.Dialogs.Commands.Update;
 
-internal sealed class UpdateDialogDataLoader : TypedDataLoader<UpdateDialogCommand, UpdateDialogResult, DialogEntity, UpdateDialogDataLoader>
+internal sealed class UpdateDialogDataLoader(FullDialogAggregateDataLoader fullDialogDataLoader) : TypedDataLoader<UpdateDialogCommand, UpdateDialogResult, DialogEntity, UpdateDialogDataLoader>
 {
-    private readonly FullDialogAggregateDataLoader _fullDialogDataLoader;
-    public UpdateDialogDataLoader(FullDialogAggregateDataLoader fullDialogDataLoader)
-    {
-        _fullDialogDataLoader = fullDialogDataLoader;
-    }
-
-    public override async Task<DialogEntity?> Load(UpdateDialogCommand request, CancellationToken cancellationToken)
-    {
-        return await _fullDialogDataLoader.LoadDialogEntity(request.Id, cancellationToken);
-    }
+    public override Task<DialogEntity?> Load(UpdateDialogCommand request, CancellationToken cancellationToken) =>
+        fullDialogDataLoader.LoadDialogEntity(request.Id, cancellationToken);
 }

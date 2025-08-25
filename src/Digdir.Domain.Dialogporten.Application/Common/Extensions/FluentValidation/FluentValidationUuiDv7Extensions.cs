82d85b0aecb31ee7d49076a99eee72a5812b91bb
Ole Jørgen Skogstad <skogstad@softis.net>
2025-08-25 13:06:48 +0200

    Update src/Digdir.Domain.Dialogporten.Application/Common/Extensions/FluentValidation/FluentValidationUuiDv7Extensions.cs
    Co-authored-by: Magnus Sandgren <5285192+MagnusSandgren@users.noreply.github.com>

--- src/Digdir.Domain.Dialogporten.Application/Common/Extensions/FluentValidation/FluentValidationUuiDv7Extensions.cs
+++ src/Digdir.Domain.Dialogporten.Application/Common/Extensions/FluentValidation/FluentValidationUuiDv7Extensions.cs
@@ -42,7 +42,7 @@ internal sealed class IsValidUuidV7TimestampValidator<T> : PropertyValidator<T, 
         context.MessageFormatter.AppendArgument("date", date.ToString("o"));
 
         // Allow up to 100ms in the future
-        return date < DateTimeOffset.UtcNow.AddMilliseconds(100);
+        return date < DateTimeOffset.UtcNow.AddSeconds(15);
     }
 
     public override string Name { get; } = "Uuid7TimestampValidator";

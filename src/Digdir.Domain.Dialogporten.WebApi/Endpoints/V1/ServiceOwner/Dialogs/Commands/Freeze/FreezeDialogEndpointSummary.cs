3af871ebeff140653a82b6a13ba2b194a7b9f8da
Amund Myrbostad <Lullinj98@gmail.com>
2025-09-02 15:01:50 +0200

    Started to fill out Summary

--- src/Digdir.Domain.Dialogporten.WebApi/Endpoints/V1/ServiceOwner/Dialogs/Commands/Freeze/FreezeDialogEndpointSummary.cs
+++ src/Digdir.Domain.Dialogporten.WebApi/Endpoints/V1/ServiceOwner/Dialogs/Commands/Freeze/FreezeDialogEndpointSummary.cs
@@ -1,6 +1,20 @@
+using Digdir.Domain.Dialogporten.WebApi.Common;
+using Digdir.Domain.Dialogporten.WebApi.Common.Extensions;
+using FastEndpoints;
+
 namespace Digdir.Domain.Dialogporten.WebApi.Endpoints.V1.ServiceOwner.Dialogs.Commands.Freeze;
 
-public class FreezeDialogEndpointSummary
+public sealed class FreezeDialogEndpointSummary : Summary<FreezeDialogEndpoint>
 {
+    public FreezeDialogEndpointSummary()
+    {
+        Summary = "Freezes a dialog";
+        Description = """
+                      Freezes a given dialog
+                      
+                      the dialog cannot be updated/deleted by the service owner (but can still be altered via admin-scope) when frozen
+                      """;
+        Responses[StatusCodes.Status204NoContent] = Constants.SwaggerSummary.Frozen.FormatInvariant("aggregate");
+    }
 
 }

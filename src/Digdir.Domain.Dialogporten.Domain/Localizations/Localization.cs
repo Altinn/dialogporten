42fa3201cc307392dcedf2adaa4f013e262babc5
Amund Myrbostad <Lullinj98@gmail.com>
2025-09-18 10:15:01 +0200

    Now uses static list of langCodes

--- src/Digdir.Domain.Dialogporten.Domain/Localizations/Localization.cs
+++ src/Digdir.Domain.Dialogporten.Domain/Localizations/Localization.cs
@@ -4,8 +4,39 @@ namespace Digdir.Domain.Dialogporten.Domain.Localizations;
 
 public sealed class Localization
 {
-    private static readonly Dictionary<string, CultureInfo> NeutralCultureByValidCultureCodes =
-        BuildNeutralCultureByValidCultureCodes();
+
+    private static readonly HashSet<string> Codes = new(StringComparer.OrdinalIgnoreCase)
+    {
+        "aa", "ab", "ae", "af", "ak", "am", "an", "ar", "as", "av", "ay", "az",
+        "ba", "be", "bg", "bh", "bi", "bm", "bn", "bo", "br", "bs",
+        "ca", "ce", "ch", "co", "cr", "cs", "cu", "cv", "cy",
+        "da", "de", "dv", "dz",
+        "ee", "el", "en", "eo", "es", "et", "eu",
+        "fa", "ff", "fi", "fj", "fo", "fr", "fy",
+        "ga", "gd", "gl", "gn", "gu", "gv",
+        "ha", "he", "hi", "ho", "hr", "ht", "hu", "hy", "hz",
+        "ia", "id", "ie", "ig", "ii", "ik", "io", "is", "it", "iu",
+        "ja", "jv",
+        "ka", "kg", "ki", "kj", "kk", "kl", "km", "kn", "ko", "kr", "ks", "ku", "kv", "kw", "ky",
+        "la", "lb", "lg", "li", "ln", "lo", "lt", "lu", "lv",
+        "mg", "mh", "mi", "mk", "ml", "mn", "mr", "ms", "mt", "my",
+        "na", "nb", "nd", "ne", "ng", "nl", "nn", "nr", "nv", "ny",
+        "oc", "oj", "om", "or", "os",
+        "pa", "pi", "pl", "ps", "pt",
+        "qu",
+        "rm", "rn", "ro", "ru", "rw",
+        "sa", "sc", "sd", "se", "sg", "si", "sk", "sl", "sm", "sn", "so", "sq", "sr", "ss", "st", "su", "sv", "sw",
+        "ta", "te", "tg", "th", "ti", "tk", "tl", "tn", "to", "tr", "ts", "tt", "tw", "ty",
+        "ug", "uk", "ur", "uz",
+        "ve", "vi", "vo",
+        "wa", "wo",
+        "xh",
+        "yi", "yo",
+        "za", "zh", "zu"
+    };
+
+    public static bool IsValidCultureCode(string? languageCode) =>
+        languageCode is not null && Codes.Contains(languageCode);
 
     private string _languageCode = null!;
 
@@ -21,43 +52,10 @@ public sealed class Localization
     public Guid LocalizationSetId { get; set; }
     public LocalizationSet LocalizationSet { get; set; } = null!;
 
-
     public static string? NormalizeCultureCode(string? cultureCode)
     {
-        cultureCode = cultureCode?.Trim().Replace('_', '-').ToLowerInvariant();
-        return cultureCode is not null && NeutralCultureByValidCultureCodes.TryGetValue(cultureCode, out var neutralCulture)
-            ? neutralCulture.TwoLetterISOLanguageName
-            : cultureCode;
+        cultureCode = cultureCode?.Trim().Replace('_', '-').ToLowerInvariant().Split('-').FirstOrDefault();
+        return cultureCode;
     }
 
-    public static bool IsValidCultureCode(string? cultureCode) =>
-        cultureCode is not null
-        && NeutralCultureByValidCultureCodes.TryGetValue(cultureCode, out var neutralCulture)
-        && cultureCode == neutralCulture.TwoLetterISOLanguageName;
-
-    private static Dictionary<string, CultureInfo> BuildNeutralCultureByValidCultureCodes()
-    {
-        var exclude = new[] { "no", "iv" };
-        var cultureGroups = CultureInfo
-            .GetCultures(CultureTypes.NeutralCultures | CultureTypes.SpecificCultures)
-            .Where(x => !exclude.Contains(x.TwoLetterISOLanguageName))
-            .GroupBy(x => x.TwoLetterISOLanguageName)
-            .ToList();
-
-        var neutralCultureByValidCultureCodes = new Dictionary<string, CultureInfo>(StringComparer.OrdinalIgnoreCase);
-
-        foreach (var cultureGroup in cultureGroups)
-        {
-            var neutral = cultureGroup.First(x => x.CultureTypes.HasFlag(CultureTypes.NeutralCultures));
-            neutralCultureByValidCultureCodes[neutral.TwoLetterISOLanguageName] = neutral;
-            neutralCultureByValidCultureCodes[neutral.ThreeLetterISOLanguageName] = neutral;
-
-            foreach (var culture in cultureGroup.Except([neutral]))
-            {
-                neutralCultureByValidCultureCodes[culture.Name] = neutral;
-            }
-        }
-
-        return neutralCultureByValidCultureCodes;
-    }
 }

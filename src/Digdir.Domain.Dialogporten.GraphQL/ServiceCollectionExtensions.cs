e11e1705b7280d26caa07c72b688d853cbc2a279
Ole Jørgen Skogstad <skogstad@softis.net>
2025-10-02 09:45:53 +0200

    fix(graphql): Add missing SetSystemLabel errors to schema (#2805)
    <!--- Provide a general summary of your changes in the Title above -->
    
    ## Description
    
    <!--- Describe your changes in detail -->
    
    ## Related Issue(s)
    
    - #2804 
    
    ## Verification
    
    - [x] **Your** code builds clean without any errors or warnings
    - [ ] Manual testing done (required)
    - [ ] Relevant automated test added (if you find this hard, leave it and
    we'll help out)
    
    ## Documentation
    
    - [ ] Documentation is updated (either in `docs`-directory, Altinnpedia
    or a separate linked PR in
    [altinn-studio-docs.](https://github.com/Altinn/altinn-studio-docs), if
    applicable)
    
    Co-authored-by: Amund Myrbostad <Lullinj98@gmail.com>

--- src/Digdir.Domain.Dialogporten.GraphQL/ServiceCollectionExtensions.cs
+++ src/Digdir.Domain.Dialogporten.GraphQL/ServiceCollectionExtensions.cs
@@ -2,42 +2,25 @@ using AppAny.HotChocolate.FluentValidation;
 using Digdir.Domain.Dialogporten.GraphQL.EndUser;
 using Digdir.Domain.Dialogporten.GraphQL.EndUser.DialogById;
 using Digdir.Domain.Dialogporten.GraphQL.EndUser.MutationTypes;
-using Digdir.Domain.Dialogporten.GraphQL.EndUser.SearchDialogs;
 using Digdir.Domain.Dialogporten.Infrastructure.Persistence;
 
 namespace Digdir.Domain.Dialogporten.GraphQL;
 
 public static class ServiceCollectionExtensions
 {
-    public static IServiceCollection AddDialogportenGraphQl(this IServiceCollection services)
-    {
-        return services
-            .AddGraphQLServer()
-            .ModifyCostOptions(o => o.ApplyCostDefaults = false)
-            // This assumes that subscriptions have been set up by the infrastructure
-            .AddSubscriptionType<Subscriptions>()
-            .AddAuthorization()
-            .RegisterDbContextFactory<DialogDbContext>()
-            .AddFluentValidation()
-            .AddQueryType<Queries>()
-            .AddMutationType<Mutations>()
-            .AddType<DialogByIdDeleted>()
-            .AddType<DialogByIdNotFound>()
-            .AddType<DialogByIdForbidden>()
-            .AddType<DialogByIdForbiddenAuthLevelTooLow>()
-            .AddType<SearchDialogValidationError>()
-            .AddType<SearchDialogForbidden>()
-            .AddType<SetSystemLabelEntityNotFound>()
-            .AddType<BulkSetSystemLabelNotFound>()
-            .AddType<BulkSetSystemLabelDomainError>()
-            .AddType<BulkSetSystemLabelValidationError>()
-            .AddType<BulkSetSystemLabelConcurrencyError>()
-            .AddType<SearchDialogOrderByParsingError>()
-            .AddType<SearchDialogContinuationTokenParsingError>()
-            .AddType<DialogByIdNotVisible>()
-            .AddMaxExecutionDepthRule(12)
-            .AddInstrumentation()
-            .InitializeOnStartup()
-            .Services;
-    }
+    public static IServiceCollection AddDialogportenGraphQl(this IServiceCollection services) => services
+        .AddGraphQLServer()
+        .ModifyCostOptions(o => o.ApplyCostDefaults = false)
+        // This assumes that subscriptions have been set up by the infrastructure
+        .AddSubscriptionType<Subscriptions>()
+        .AddAuthorization()
+        .RegisterDbContextFactory<DialogDbContext>()
+        .AddFluentValidation()
+        .AddQueryType<Queries>()
+        .AddMutationType<Mutations>()
+        .AddErrorTypes()
+        .AddMaxExecutionDepthRule(12)
+        .AddInstrumentation()
+        .InitializeOnStartup()
+        .Services;
 }

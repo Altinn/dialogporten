ea39907063faf23ee137fac5fc4c8756c23631a6
Ole Jørgen Skogstad <skogstad@softis.net>
2025-10-02 09:34:42 +0200

    add

--- src/Digdir.Domain.Dialogporten.GraphQL/ServiceCollectionExtensions.cs
+++ src/Digdir.Domain.Dialogporten.GraphQL/ServiceCollectionExtensions.cs
@@ -4,14 +4,14 @@ using Digdir.Domain.Dialogporten.GraphQL.EndUser.DialogById;
 using Digdir.Domain.Dialogporten.GraphQL.EndUser.MutationTypes;
 using Digdir.Domain.Dialogporten.GraphQL.EndUser.SearchDialogs;
 using Digdir.Domain.Dialogporten.Infrastructure.Persistence;
+using HotChocolate.Execution.Configuration;
 
 namespace Digdir.Domain.Dialogporten.GraphQL;
 
 public static class ServiceCollectionExtensions
 {
     public static IServiceCollection AddDialogportenGraphQl(this IServiceCollection services)
-    {
-        return services
+    => services
             .AddGraphQLServer()
             .ModifyCostOptions(o => o.ApplyCostDefaults = false)
             // This assumes that subscriptions have been set up by the infrastructure
@@ -21,23 +21,31 @@ public static class ServiceCollectionExtensions
             .AddFluentValidation()
             .AddQueryType<Queries>()
             .AddMutationType<Mutations>()
-            .AddType<DialogByIdDeleted>()
-            .AddType<DialogByIdNotFound>()
-            .AddType<DialogByIdForbidden>()
-            .AddType<DialogByIdForbiddenAuthLevelTooLow>()
-            .AddType<SearchDialogValidationError>()
-            .AddType<SearchDialogForbidden>()
-            .AddType<SetSystemLabelEntityNotFound>()
-            .AddType<BulkSetSystemLabelNotFound>()
-            .AddType<BulkSetSystemLabelDomainError>()
-            .AddType<BulkSetSystemLabelValidationError>()
-            .AddType<BulkSetSystemLabelConcurrencyError>()
-            .AddType<SearchDialogOrderByParsingError>()
-            .AddType<SearchDialogContinuationTokenParsingError>()
-            .AddType<DialogByIdNotVisible>()
+            .AddErrorTypes()
             .AddMaxExecutionDepthRule(12)
             .AddInstrumentation()
             .InitializeOnStartup()
             .Services;
+}
+
+internal static class IRequestExecutorBuilderExtensions
+{
+    internal static IRequestExecutorBuilder AddErrorTypes(this IRequestExecutorBuilder builder)
+    {
+        var errorInterfaces = new[]
+        {
+            typeof(IBulkSetSystemLabelError),
+            typeof(ISetSystemLabelError),
+            typeof(ISearchDialogError),
+            typeof(IDialogByIdError)
+        };
+
+        var errorTypes = typeof(ServiceCollectionExtensions).Assembly
+            .GetTypes()
+            .Where(t => !t.IsAbstract && errorInterfaces
+                .Any(i => i.IsAssignableFrom(t)) && t.IsClass)
+            .ToList();
+
+        return errorTypes.Aggregate(builder, (current, type) => current.AddType(type));
     }
 }

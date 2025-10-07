e13c2d19401d2fe10de6622e87c04717b45ac346
Ole Jørgen Skogstad <skogstad@softis.net>
2025-10-07 13:21:50 +0200

    fix(app): Use org consumer claim for feature metrics (#2823)
    <!--- Provide a general summary of your changes in the Title above -->
    
    ## Description
    
    <!--- Describe your changes in detail -->
    
    ## Related Issue(s)
    
    - #2791
    
    ## Verification
    
    - [x] **Your** code builds clean without any errors or warnings
    - [ ] Manual testing done (required)
    - [ ] Relevant automated test added (if you find this hard, leave it and
    we'll help out)
    
    ## Documentation
    
    - [ ] Documentation is updated (either in `docs`-directory, Altinnpedia
    or a separate linked PR in
    [altinn-studio-docs.](https://github.com/Altinn/altinn-studio-docs), if
    applicable)
    
    ---------
    
    Co-authored-by: Knut Haug <154342485+knuhau@users.noreply.github.com>

--- src/Digdir.Domain.Dialogporten.Application/Common/Behaviours/FeatureMetric/FeatureMetricBehaviour.cs
+++ src/Digdir.Domain.Dialogporten.Application/Common/Behaviours/FeatureMetric/FeatureMetricBehaviour.cs
@@ -24,16 +24,18 @@ internal sealed class FeatureMetricBehaviour<TRequest, TResponse>(
         CancellationToken cancellationToken)
     {
         var principal = _user.GetPrincipal();
-        principal.TryGetOrganizationShortName(out var performingOrg);
+        principal.TryGetConsumerOrgNumber(out var performingOrgNr);
+
         var hasAdminScope = principal.HasScope(AuthorizationScope.ServiceOwnerAdminScope);
         var resource = await _featureMetricServiceResourceResolver.Resolve(request, cancellationToken);
+
         _featureMetricRecorder.Record(new(
             FeatureName: typeof(TRequest).FullName!,
             HasAdminScope: hasAdminScope,
             Environment: hostEnvironment?.EnvironmentName,
-            PerformerOrg: performingOrg,
-            OwnerOrg: resource?.OwnOrgShortName,
+            PerformerOrg: performingOrgNr,
             ServiceResource: resource?.ResourceId));
+
         return await next(cancellationToken);
     }
 }

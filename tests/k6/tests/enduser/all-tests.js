4da9a05dbbea88ded9e0f187c463c29878dc15fd
Amund <amund.myrbostad@digdir.no>
2025-10-07 13:19:09 +0200

    feat: add support for accept-language header (#2747)
    <!--- Provide a general summary of your changes in the Title above -->
    
    ## Description
    
    EU GET endpoints now support Accept-Language Header
    
    ## Related Issue(s)
    
    - #2198 
    
    ## Verification
    
    - [x] **Your** code builds clean without any errors or warnings
    - [x] Manual testing done (required)
    - [x] Relevant automated test added (if you find this hard, leave it and
    we'll help out)
    
    ## Documentation
    
    - [ ] Documentation is updated (either in `docs`-directory, Altinnpedia
    or a separate linked PR in
    [altinn-studio-docs.](https://github.com/Altinn/altinn-studio-docs), if
    applicable)
    
    ---------
    
    Co-authored-by: Ole Jørgen Skogstad <skogstad@softis.net>

--- tests/k6/tests/enduser/all-tests.js
+++ tests/k6/tests/enduser/all-tests.js
@@ -1,4 +1,5 @@
 // This file is generated, see "scripts" directory
+import { default as DialogGetAcceptLanguage } from './DialogGetAcceptLanguage.js';
 import { default as dialogBulkSystemLabels } from './dialogBulkSystemLabels.js';
 import { default as dialogDetails } from './dialogDetails.js';
 import { default as dialogSearch } from './dialogSearch.js';
@@ -7,6 +8,7 @@ import { default as dialogVisibleFrom } from './dialogVisibleFrom.js';
 import { default as parties } from './parties.js';
 
 export default function() {
+  DialogGetAcceptLanguage();
   dialogBulkSystemLabels();
   dialogDetails();
   dialogSearch();

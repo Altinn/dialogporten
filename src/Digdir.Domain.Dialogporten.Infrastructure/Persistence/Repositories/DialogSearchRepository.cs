55be6cdff67e34daeed2f36b02cd5e01d7aeee70
Magnus Sandgren <5285192+MagnusSandgren@users.noreply.github.com>
2025-10-09 11:25:36 +0200

    Consider dialogs with no content in indexer.

--- src/Digdir.Domain.Dialogporten.Infrastructure/Persistence/Repositories/DialogSearchRepository.cs
+++ src/Digdir.Domain.Dialogporten.Infrastructure/Persistence/Repositories/DialogSearchRepository.cs
@@ -15,65 +15,66 @@ internal sealed class DialogSearchRepository(DialogDbContext db) : IDialogSearch
     private const string FreeTextSearchIndexerSql =
         """
         WITH dialocContent AS (
-           -- Dialog Content
-           SELECT dc."DialogId" dialogId
-               ,CASE dc."TypeId"
-                   WHEN 1 THEN 'B' -- title
-                   ELSE 'D'
-               END weight
-               ,l."LanguageCode" languageCode
-               ,l."Value" value
-           FROM "DialogContent" dc
-           INNER JOIN "LocalizationSet" dcls ON dc."Id" = dcls."DialogContentId"
-           INNER JOIN "Localization" l ON dcls."Id" = l."LocalizationSetId"
-           WHERE dc."MediaType" = 'text/plain'
-           
-           -- Transmission content
-           UNION ALL SELECT dt."DialogId"
-              ,'D' weight
-              ,l."LanguageCode" languageCode
-              ,l."Value" value
-           FROM "DialogTransmission" dt
-           INNER JOIN "DialogTransmissionContent" dtc ON dt."Id" = dtc."TransmissionId"
-           INNER JOIN "LocalizationSet" dcls ON dtc."Id" = dcls."TransmissionContentId"
-           INNER JOIN "Localization" l ON dcls."Id" = l."LocalizationSetId"
-           WHERE dtc."MediaType" = 'text/plain'
-           
-           -- Activity description
-           UNION ALL SELECT da."DialogId"
-              ,'D' weight
-              ,l."LanguageCode" languageCode
-              ,l."Value" value
-           FROM "DialogActivity" da
-           INNER JOIN "LocalizationSet" dcls ON da."Id" = dcls."ActivityId"
-           INNER JOIN "Localization" l ON dcls."Id" = l."LocalizationSetId"
-           
-           -- Attachment description (can be linked to dialog or transmission)
-           UNION ALL SELECT DISTINCT coalesce(a."DialogId", dt."DialogId") dialogId
+            -- Dialog Content
+            SELECT dc."DialogId" dialogId
+                ,CASE dc."TypeId"
+                    WHEN 1 THEN 'B' -- title
+                    ELSE 'D'
+                END weight
+                ,l."LanguageCode" languageCode
+                ,l."Value" value
+            FROM "DialogContent" dc
+            INNER JOIN "LocalizationSet" dcls ON dc."Id" = dcls."DialogContentId"
+            INNER JOIN "Localization" l ON dcls."Id" = l."LocalizationSetId"
+            WHERE dc."MediaType" = 'text/plain'
+            
+            -- Transmission content
+            UNION ALL SELECT dt."DialogId"
                ,'D' weight
                ,l."LanguageCode" languageCode
                ,l."Value" value
-           FROM "Attachment" a
-           LEFT JOIN "DialogTransmission" dt ON a."TransmissionId" = dt."Id"
-           INNER JOIN "LocalizationSet" dcls ON a."Id" = dcls."AttachmentId"
-           INNER JOIN "Localization" l ON dcls."Id" = l."LocalizationSetId"
+            FROM "DialogTransmission" dt
+            INNER JOIN "DialogTransmissionContent" dtc ON dt."Id" = dtc."TransmissionId"
+            INNER JOIN "LocalizationSet" dcls ON dtc."Id" = dcls."TransmissionContentId"
+            INNER JOIN "Localization" l ON dcls."Id" = l."LocalizationSetId"
+            WHERE dtc."MediaType" = 'text/plain'
+            
+            -- Activity description
+            UNION ALL SELECT da."DialogId"
+               ,'D' weight
+               ,l."LanguageCode" languageCode
+               ,l."Value" value
+            FROM "DialogActivity" da
+            INNER JOIN "LocalizationSet" dcls ON da."Id" = dcls."ActivityId"
+            INNER JOIN "Localization" l ON dcls."Id" = l."LocalizationSetId"
+            
+            -- Attachment description (can be linked to dialog or transmission)
+            UNION ALL SELECT DISTINCT coalesce(a."DialogId", dt."DialogId") dialogId
+                ,'D' weight
+                ,l."LanguageCode" languageCode
+                ,l."Value" value
+            FROM "Attachment" a
+            LEFT JOIN "DialogTransmission" dt ON a."TransmissionId" = dt."Id"
+            INNER JOIN "LocalizationSet" dcls ON a."Id" = dcls."AttachmentId"
+            INNER JOIN "Localization" l ON dcls."Id" = l."LocalizationSetId"
+        ), aggregatedVectorizedDialogContent AS (
+            SELECT d."Id" AS dialogId
+                ,string_agg(
+                    setweight(
+                        to_tsvector(COALESCE(isomap."TsConfigName", 'simple')::regconfig, value),
+                        weight::"char")::text,
+                    ' ')::tsvector AS document
+            FROM "Dialog" d -- ensure we get a row even if no content (only if dialog exists)
+            LEFT JOIN dialocContent dc ON d."Id" = dc.dialogId
+            LEFT JOIN search."Iso639TsVectorMap" isomap ON dc.languageCode = isomap."IsoCode"
+            GROUP BY d."Id"
         )
         INSERT INTO search."DialogSearch" ("DialogId", "UpdatedAt", "SearchVector")
-        SELECT dialogId,
-           now() "UpdatedAt",
-           string_agg(
-               setweight(
-                   to_tsvector(COALESCE(ismap."TsConfigName", 'simple')::regconfig, value),
-                   weight::"char"
-               )::text,
-           ' '
-           )::tsvector document
-        FROM dialocContent
-        LEFT JOIN search."Iso639TsVectorMap" ismap ON dialocContent.languageCode = ismap."IsoCode"
+        SELECT dialogId, now(), document
+        FROM aggregatedVectorizedDialogContent
         WHERE dialogId = {0}
-        GROUP BY dialogId
         ON CONFLICT ("DialogId") DO UPDATE
         SET "UpdatedAt" = EXCLUDED."UpdatedAt",
-           "SearchVector" = EXCLUDED."SearchVector";
+            "SearchVector" = EXCLUDED."SearchVector";
         """;
 }

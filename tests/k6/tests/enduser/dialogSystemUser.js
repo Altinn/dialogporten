24e05b967c7a0e82503394c90df6270341c93a9b
Amund Myrbostad <Lullinj98@gmail.com>
2025-10-09 10:58:45 +0200

    Created test for search

--- tests/k6/tests/enduser/dialogSystemUser.js
+++ tests/k6/tests/enduser/dialogSystemUser.js
@@ -3,12 +3,15 @@ import {
     getEU,
     getSysEU,
     setVisibleFrom,
+    setTitle,
     postSO,
-    purgeSO } from '../../common/testimports.js'
+    purgeSO
+} from '../../common/testimports.js'
+import {getDefaultEnduserSsn} from '../../common/token.js';
+import {default as dialogToInsert} from '../serviceowner/testdata/01-create-dialog.js';
 
-import { default as dialogToInsert } from '../serviceowner/testdata/01-create-dialog.js';
-export  default function (){
-    describe('SystemUser with empty party list', () => {
+export default function () {
+    describe('Get Single Dialog with SystemUser with empty party list', () => {
         // Arrange
         let dialog = dialogToInsert();
         setVisibleFrom(dialog, null);
@@ -16,15 +19,35 @@ export  default function (){
         let r = postSO("dialogs", dialog);
         expectStatusFor(r).to.equal(201);
         let dialogId = r.json();
-        
-        
+
         // Assert
-        // let getRes = getEU('dialogs/' + dialogId);
-        // expectStatusFor(getRes).to.equal(200);
+        let getResponse = getSysEU('dialogs/' + dialogId);
+        expectStatusFor(getResponse).to.equal(404);
+
+        // Clean up
+        let purgeResponse = purgeSO("dialogs/" + dialogId);
+        expect(purgeResponse.status, 'response status').to.equal(204);
+    });
 
 
-        let getResSys = getSysEU('dialogs/' + dialogId);
-        expectStatusFor(getResSys).to.equal(404);
+    describe('SystemUser with empty party list', () => {
+        // Arrange
+        let dialog = dialogToInsert();
+        let title = "system titles";
+        let defaultParty = "urn:altinn:person:identifier-no:" + getDefaultEnduserSsn();
+        setVisibleFrom(dialog, null);
+        setTitle(dialog, title);
+
+        let r = postSO("dialogs", dialog);
+        expectStatusFor(r).to.equal(201);
+        let dialogId = r.json();
+
+        // Assert
+        let getResponse = getSysEU(`dialogs/?&Search=${title}&Party=${defaultParty}`);
+        expectStatusFor(getResponse).to.equal(200);
+        expect(getResponse, 'response').to.have.validJsonBody();
+        expect(getResponse.json(), 'response json').to.not.have.property('items')
+
         // Clean up
         let purgeResponse = purgeSO("dialogs/" + dialogId);
         expect(purgeResponse.status, 'response status').to.equal(204);

279847c9b493eda920c5d5d00e93b927688a19e0
Ole Jørgen Skogstad <skogstad@softis.net>
2025-10-31 10:23:29 +0100

    fix(api): add missing ExternalReferences (#2944)
    <!--- Provide a general summary of your changes in the Title above -->
    
    ## Description
    
    <!--- Describe your changes in detail -->
    
    ## Related Issue(s)
    
    - #2941 
    
    ## Verification
    
    - [ ] **Your** code builds clean without any errors or warnings
    - [ ] Manual testing done (required)
    - [ ] Relevant automated test added (if you find this hard, leave it and
    we'll help out)
    
    ## Documentation
    
    - [ ] Documentation is updated (either in `docs`-directory, Altinnpedia
    or a separate linked PR in
    [altinn-studio-docs.](https://github.com/Altinn/altinn-studio-docs), if
    applicable)

---
+++ tests/Digdir.Domain.Dialogporten.Application.Integration.Tests/Features/V1/EndUser/Transmissions/Queries/Search/SearchTransmissionsTests.cs
@@ -0,0 +1,24 @@
+using Digdir.Domain.Dialogporten.Application.Features.V1.EndUser.Dialogs.Queries.SearchTransmissions;
+using Digdir.Domain.Dialogporten.Application.Integration.Tests.Common;
+using Digdir.Domain.Dialogporten.Application.Integration.Tests.Common.ApplicationFlow;
+using Digdir.Domain.Dialogporten.Application.Integration.Tests.Features.V1.Common;
+using FluentAssertions;
+
+namespace Digdir.Domain.Dialogporten.Application.Integration.Tests.Features.V1.EndUser.Transmissions.Queries.Search;
+
+[Collection(nameof(DialogCqrsCollectionFixture))]
+public class SearchTransmissionsTests(DialogApplication application) : ApplicationCollectionFixture(application)
+{
+    [Fact]
+    public Task Search_Transmission_Should_Include_ExternalReference() =>
+        FlowBuilder.For(Application)
+            .CreateSimpleDialog(x =>
+                x.AddTransmission(x => x.ExternalReference = "ext"))
+            .SendCommand((_, ctx) => new SearchTransmissionQuery
+            {
+                DialogId = ctx.GetDialogId()
+            })
+            .ExecuteAndAssert<List<TransmissionDto>>(x =>
+                x.Should().ContainSingle().Which
+                    .ExternalReference.Should().Be("ext"));
+}

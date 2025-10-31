a947ac01bdd7a1eac5dd9c1f4135bd52b2b34f73
Knut Haug <154342485+knuhau@users.noreply.github.com>
2025-10-31 12:06:31 +0100

    chore(app): Add FusionCacheFilter to OpenTelemetry processors (#2943)
    ## Related Issue(s)
    
    - #2942
    
    ---------
    
    Co-authored-by: Ole Jørgen Skogstad <skogstad@softis.net>

--- src/Digdir.Library.Utils.AspNet/HealthCheckFilter.cs
+++ src/Digdir.Library.Utils.AspNet/HealthCheckFilter.cs
@@ -6,6 +6,12 @@ public class HealthCheckFilter : OpenTelemetry.BaseProcessor<Activity>
 {
     public override void OnEnd(Activity activity)
     {
+        if (!activity.Source.Name.StartsWith(Constants.AspNetCore, StringComparison.OrdinalIgnoreCase))
+        {
+            base.OnEnd(activity);
+            return;
+        }
+
         var requestPath = activity.Tags.FirstOrDefault(t => t.Key == "http.route").Value;
         if ((requestPath?.EndsWith("/health", StringComparison.InvariantCultureIgnoreCase) ?? false)
          || (requestPath?.EndsWith("/health/deep", StringComparison.InvariantCultureIgnoreCase) ?? false))

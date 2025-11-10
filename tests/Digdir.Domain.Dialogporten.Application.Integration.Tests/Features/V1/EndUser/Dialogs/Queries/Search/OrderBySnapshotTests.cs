1d05d39c3888603dd87d2e6cfc8da7abf7b231e8
Ole Jørgen Skogstad <skogstad@softis.net>
2025-11-10 18:30:22 +0100

    orderby snapshot test

--- tests/Digdir.Domain.Dialogporten.Application.Integration.Tests/Features/V1/EndUser/Dialogs/Queries/Search/OrderBySnapshotTests.cs
+++ tests/Digdir.Domain.Dialogporten.Application.Integration.Tests/Features/V1/EndUser/Dialogs/Queries/Search/OrderBySnapshotTests.cs
@@ -2,6 +2,7 @@ using Digdir.Domain.Dialogporten.Application.Common.Pagination;
 using Digdir.Domain.Dialogporten.Application.Common.Pagination.Continuation;
 using Digdir.Domain.Dialogporten.Application.Common.Pagination.Order;
 using Digdir.Domain.Dialogporten.Application.Features.V1.EndUser.Dialogs.Queries.Search;
+using Digdir.Domain.Dialogporten.Application.Features.V1.ServiceOwner.Dialogs.Commands.Create;
 using Digdir.Domain.Dialogporten.Application.Integration.Tests.Common;
 using Digdir.Domain.Dialogporten.Application.Integration.Tests.Common.ApplicationFlow;
 using Digdir.Domain.Dialogporten.Application.Integration.Tests.Features.V1.Common;
@@ -12,97 +13,22 @@ namespace Digdir.Domain.Dialogporten.Application.Integration.Tests.Features.V1.E
 [Collection(nameof(DialogCqrsCollectionFixture))]
 public class OrderBySnapshotTests(DialogApplication application) : ApplicationCollectionFixture(application)
 {
-    private static DateTimeOffset DateAnchor => new(2023, 1, 1, 0, 0, 0, TimeSpan.Zero);
     [Theory]
     [InlineData("createdAt")]
+    [InlineData("createdAt_asc")]
+    [InlineData("updatedAt")]
+    [InlineData("updatedAt_asc")]
+    [InlineData("contentUpdatedAt")]
+    [InlineData("contentUpdatedAt_asc")]
+    [InlineData("dueAt")]
+    [InlineData("dueAt_asc")]
     public async Task OrderBy_Search_Verify_Output(string orderBy)
     {
         await FlowBuilder.For(Application)
-            .CreateSimpleDialog(x =>
-            {
-                x.Dto = SnapshotDialog.Create();
-                x.Dto.Activities.Clear();
-                x.Dto.Party = IntegrationTestUser.DefaultParty;
-                x.Dto.CreatedAt = x.Dto.UpdatedAt = DateAnchor;
-            })
-            .CreateSimpleDialog(x =>
-            {
-                x.Dto = SnapshotDialog.Create();
-                x.Dto.Activities.Clear();
-                x.Dto.Party = IntegrationTestUser.DefaultParty;
-                x.Dto.CreatedAt = x.Dto.UpdatedAt = DateAnchor.AddDays(1);
-            })
-            .CreateSimpleDialog(x =>
-            {
-                x.Dto = SnapshotDialog.Create();
-                x.Dto.Activities.Clear();
-                x.Dto.Party = IntegrationTestUser.DefaultParty;
-                x.Dto.CreatedAt = x.Dto.UpdatedAt = DateAnchor.AddDays(2);
-            })
-            .CreateSimpleDialog(x =>
-            {
-                x.Dto = SnapshotDialog.Create();
-                x.Dto.Activities.Clear();
-                x.Dto.Party = IntegrationTestUser.DefaultParty;
-                x.Dto.CreatedAt = x.Dto.UpdatedAt = DateAnchor.AddDays(3);
-            })
-            .CreateSimpleDialog(x =>
-            {
-                x.Dto = SnapshotDialog.Create();
-                x.Dto.Activities.Clear();
-                x.Dto.Party = IntegrationTestUser.DefaultParty;
-                x.Dto.CreatedAt = x.Dto.UpdatedAt = DateAnchor.AddDays(4);
-            })
-            .CreateSimpleDialog(x =>
-            {
-                x.Dto = SnapshotDialog.Create();
-                x.Dto.Activities.Clear();
-                x.Dto.Party = IntegrationTestUser.DefaultParty;
-                x.Dto.CreatedAt = x.Dto.UpdatedAt = DateAnchor.AddDays(5);
-            })
-            .CreateSimpleDialog(x =>
-            {
-                x.Dto = SnapshotDialog.Create();
-                x.Dto.Activities.Clear();
-                x.Dto.Party = IntegrationTestUser.DefaultParty;
-                x.Dto.CreatedAt = x.Dto.UpdatedAt = DateAnchor.AddDays(6);
-            })
+            .AddOrderByDialogs()
             .ExecuteAndAssert(_ => { });
 
-
-        var searchResultPage1 = await FlowBuilder.For(Application)
-            .SearchEndUserDialogs(x =>
-            {
-                x.Limit = 2;
-                x.Party = [IntegrationTestUser.DefaultParty];
-                x.OrderBy =
-                    OrderSet<SearchDialogQueryOrderDefinition, IntermediateDialogDto>.TryParse(orderBy,
-                        out var lala)
-                        ? lala
-                        : null;
-            })
-            .ExecuteAndAssert<PaginatedList<DialogDto>>(x =>
-            {
-                x.Items.Should().HaveCount(2);
-            });
-
-        var searchResultPage2 = await FlowBuilder.For(Application)
-            .SearchEndUserDialogs(x =>
-            {
-                x.Limit = 2;
-                x.Party = [IntegrationTestUser.DefaultParty];
-                x.ContinuationToken = ContinuationTokenSet<SearchDialogQueryOrderDefinition, IntermediateDialogDto>
-                    .TryParse(searchResultPage1.ContinuationToken, out var token) ? token : null;
-                x.OrderBy =
-                    OrderSet<SearchDialogQueryOrderDefinition, IntermediateDialogDto>.TryParse(orderBy,
-                        out var lala)
-                        ? lala
-                        : null;
-            })
-            .ExecuteAndAssert<PaginatedList<DialogDto>>(x =>
-            {
-                x.Items.Should().HaveCount(2);
-            });
+        var pages = await GetAllPages(orderBy);
 
         var settings = new VerifySettings();
 
@@ -110,8 +36,66 @@ public class OrderBySnapshotTests(DialogApplication application) : ApplicationCo
         settings.IgnoreMember(nameof(PaginatedList<DialogDto>.ContinuationToken));
         settings.UseFileName($"OrderBySnapshotTests.{orderBy}");
 
-        await Verify(searchResultPage1, settings)
+        await Verify(pages, settings)
             .UseDirectory("Snapshots")
             .DontScrubDateTimes();
+    }
+
+    private async Task<List<PaginatedList<DialogDto>>> GetAllPages(string orderBy)
+    {
+        List<PaginatedList<DialogDto>> pages = [];
+        bool hasNextPage;
+        string? continuationToken = null;
+        do
+        {
+            var previousToken = continuationToken;
+            var page = await FlowBuilder.For(Application)
+                .SearchEndUserDialogs(x =>
+                {
+                    x.Limit = 2;
+                    x.Party = [IntegrationTestUser.DefaultParty];
+                    x.ContinuationToken = ContinuationTokenSet<SearchDialogQueryOrderDefinition, IntermediateDialogDto>
+                        .TryParse(previousToken, out var token) ? token : null;
+                    x.OrderBy =
+                        OrderSet<SearchDialogQueryOrderDefinition, IntermediateDialogDto>.TryParse(orderBy,
+                            out var lala)
+                            ? lala
+                            : null;
+                })
+                .ExecuteAndAssert<PaginatedList<DialogDto>>();
+
+            pages.Add(page);
+            hasNextPage = page.HasNextPage;
+            continuationToken = page.ContinuationToken;
+        } while (hasNextPage);
+
+        return pages;
+    }
+}
+
+public static class OrderByExtensions
+{
+    private static DateTimeOffset DateAnchor => new(2023, 1, 1, 0, 0, 0, TimeSpan.Zero);
+
+    public static IFlowExecutor<CreateDialogResult> AddOrderByDialogs(this IFlowStep step)
+    {
+        IFlowExecutor<CreateDialogResult> executor = null!;
+        for (var i = 0; i < 5; i++)
+        {
+            var addDays = i;
+            executor = step.CreateSimpleDialog(x =>
+            {
+                x.Dto = SnapshotDialog.Create();
+                x.Dto.Activities.Clear();
+                x.Dto.Party = IntegrationTestUser.DefaultParty;
+
+                x.Dto.CreatedAt = DateAnchor.AddDays(addDays);
+                x.Dto.UpdatedAt = DateAnchor.AddDays(addDays);
+                // This test will break in 2123 ᕕ(⌐■_■)ᕗ ♪♬
+                x.Dto.DueAt = DateAnchor.AddYears(100 + addDays);
+            });
+        }
+
+        return executor;
     }
 }

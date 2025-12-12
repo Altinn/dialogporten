70d3935c95935ebd6acc16b18726447f1c9aa115
Magnus Sandgren <5285192+MagnusSandgren@users.noreply.github.com>
2025-12-12 15:55:06 +0100

    fix(search): Limit tsvector length in VDialogDocument view to prevent errors

--- src/Digdir.Domain.Dialogporten.Infrastructure/Persistence/Sql/Dialog/Search/View.VDialogDocument.V3.sql
+++ src/Digdir.Domain.Dialogporten.Infrastructure/Persistence/Sql/Dialog/Search/View.VDialogDocument.V3.sql
@@ -1,23 +1,21 @@
 -- Produces weighted tsvectors per dialog so upserts remain a simple INSERT ... SELECT.
 CREATE OR REPLACE VIEW search."VDialogDocument" AS
-SELECT d."Id" AS "DialogId",
-       (
-         SELECT
-           string_agg(
-             setweight(
-               to_tsvector(
-                   -- Fall back to simple when the language map lacks a match.
-                   COALESCE(isomap."TsConfigName", 'simple')::regconfig, 
-                   -- Truncate to avoid overly large documents and errors.
-                   LEFT(c."Value", 100000)), 
-               c."Weight"::"char"
-             )::text,
-             ' '
-           )::tsvector
-         FROM search."VDialogContent" c
-         LEFT JOIN search."Iso639TsVectorMap" isomap
-           ON c."LanguageCode" = isomap."IsoCode"
-         WHERE c."DialogId" = d."Id"
-       ) AS "Document",
-       d."Party" AS "Party"
+SELECT d."Id"    AS "DialogId"
+     , (
+    SELECT REGEXP_REPLACE(
+               LEFT(
+                   STRING_AGG(
+                       SETWEIGHT(
+                           -- Fall back to simple when the language map lacks a match.
+                           TO_TSVECTOR(COALESCE(isomap."TsConfigName", 'simple')::regconfig, c."Value"),
+                           c."Weight"::"char"
+                       )::text,
+                       ' '
+                   ), 1048575), E'\\s[^\\s]*$', '')::tsvector
+    FROM search."VDialogContent" c
+    LEFT JOIN search."Iso639TsVectorMap" isomap
+        ON c."LanguageCode" = isomap."IsoCode"
+    WHERE c."DialogId" = d."Id"
+)                AS "Document"
+     , d."Party" AS "Party"
 FROM "Dialog" d;

9f3c73bd4c714430651dd5d8a38313be9a9bc265
Ole Jørgen Skogstad <skogstad@softis.net>
2025-12-29 20:02:53 +0100

    --wip-- [skip ci]

---
+++ tests/Digdir.Domain.Dialogporten.GraphQl.E2E.Tests/UnitTest1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace Digdir.Domain.Dialogporten.GraphQl.E2E.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public async Task Test1()
+    {
+        var serviceCollection = new ServiceCollection();
+        serviceCollection
+            .AddDialogportenGraphQlTestClient()
+            .ConfigureHttpClient(c =>
+                c.BaseAddress = new Uri("http://localhost:5181/graphql"));
+
+        IServiceProvider services = serviceCollection.BuildServiceProvider();
+
+        var client = services.GetRequiredService<IDialogportenGraphQlTestClient>();
+
+        // var dialogId = Guid.Parse("019ad4a0-9df9-71f0-85e1-883d2b551650");
+        var dialogId = Guid.NewGuid();
+
+        var foo = await client.GetDialogById.ExecuteAsync(dialogId);
+
+        foo.Data.Should().NotBeNull();
+        foo.Data.DialogById.Errors.Should().NotBeNull();
+        foo.Data.DialogById.Errors
+            .Should().ContainSingle().Which.Message.Should().Contain(dialogId.ToString())
+            .And.GetType().Should().BeOfType(typeof(GetDialogById_DialogById_Errors_DialogByIdNotFound));
+    }
+}

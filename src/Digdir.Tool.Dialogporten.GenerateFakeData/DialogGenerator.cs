5b4b21a0222961b4b27247ec7ab01be21a00c7c7
Magnus Sandgren <5285192+MagnusSandgren@users.noreply.github.com>
2026-01-06 10:06:22 +0100

    fix(DialogGenerator): remove RefTime causing flaky DueDate and ExpiresAt.

--- src/Digdir.Tool.Dialogporten.GenerateFakeData/DialogGenerator.cs
+++ src/Digdir.Tool.Dialogporten.GenerateFakeData/DialogGenerator.cs
@@ -22,8 +22,6 @@ public static class DialogGenerator
         Randomizer.Seed = new Random(123456);
     }
 
-    private static readonly DateTime RefTime = new(2026, 1, 1);
-
     [ThreadStatic] private static Randomizer? _rnd;
     [ThreadStatic] private static Faker? _faker;
     private static Randomizer MyRandomizer => _rnd ??= new Randomizer();
@@ -91,8 +89,6 @@ public static class DialogGenerator
         .RuleFor(o => o.ExternalReference, f => f.Random.AlphaNumeric(10))
         .RuleFor(o => o.CreatedAt, _ => null)
         .RuleFor(o => o.UpdatedAt, _ => null)
-        .RuleFor(o => o.DueAt, f => f.Date.Future(10, RefTime))
-        .RuleFor(o => o.ExpiresAt, f => f.Date.Future(20, RefTime.AddYears(11)))
         .RuleFor(o => o.VisibleFrom, _ => null)
         .RuleFor(o => o.Status, f => f.PickRandom<DialogStatusInput>())
         .RuleFor(o => o.Content, _ => GenerateFakeContent())

fc9ca28ba680b908cd243bb4082197134cda9453
Ole Jørgen Skogstad <skogstad@softis.net>
2026-01-12 21:32:52 +0100

    use ctx overload for ExecuteAndAssert

--- tests/Digdir.Domain.Dialogporten.Application.Integration.Tests/Features/V1/ServiceOwner/Dialogs/Queries/SearchEndUserContext/SearchDialogEndUserContextTests.cs
+++ tests/Digdir.Domain.Dialogporten.Application.Integration.Tests/Features/V1/ServiceOwner/Dialogs/Queries/SearchEndUserContext/SearchDialogEndUserContextTests.cs
@@ -13,25 +13,20 @@ namespace Digdir.Domain.Dialogporten.Application.Integration.Tests.Features.V1.S
 public class SearchDialogEndUserContextTests(DialogApplication application) : ApplicationCollectionFixture(application)
 {
     [Fact]
-    public Task Search_Returns_EndUserContext_Labels_With_Any_Label_Match()
-    {
-        var dialogId = Guid.Empty;
-
-        return FlowBuilder.For(Application)
+    public Task Search_Returns_EndUserContext_Labels_With_Any_Label_Match() =>
+        FlowBuilder.For(Application)
             .CreateSimpleDialog(x => x.Dto.Party = IntegrationTestUser.DefaultParty)
             .SetSystemLabelsServiceOwner(x => x.AddLabels = [SystemLabel.Values.Archive])
             .SearchServiceOwnerDialogEndUserContexts((query, ctx) =>
             {
-                dialogId = ctx.GetDialogId();
                 query.Party = [ctx.GetParty()];
                 query.Label = [SystemLabel.Values.Archive, SystemLabel.Values.Bin];
             })
-            .ExecuteAndAssert<PaginatedList<DialogEndUserContextItemDto>>(result =>
+            .ExecuteAndAssert<PaginatedList<DialogEndUserContextItemDto>>((result, ctx) =>
                 result.Items.Should().ContainSingle(item =>
-                    item.DialogId == dialogId &&
+                    item.DialogId == ctx.GetDialogId() &&
                     item.EndUserContextRevision != Guid.Empty &&
                     item.SystemLabels.Contains(SystemLabel.Values.Archive)));
-    }
 
     [Fact]
     public Task Search_Without_Party_Returns_ValidationError() =>
@@ -40,9 +35,8 @@ public class SearchDialogEndUserContextTests(DialogApplication application) : Ap
             .ExecuteAndAssert<ValidationError>();
 
     [Fact]
-    public Task Search_With_EndUserId_And_No_Authorizations_Returns_Empty()
-    {
-        return FlowBuilder.For(Application, services =>
+    public Task Search_With_EndUserId_And_No_Authorizations_Returns_Empty() =>
+        FlowBuilder.For(Application, services =>
             {
                 services.ConfigureAltinnAuthorization(x =>
                     x.ConfigureGetAuthorizedResourcesForSearch(new DialogSearchAuthorizationResult()));
@@ -55,27 +49,18 @@ public class SearchDialogEndUserContextTests(DialogApplication application) : Ap
             })
             .ExecuteAndAssert<PaginatedList<DialogEndUserContextItemDto>>(result =>
                 result.Items.Should().BeEmpty());
-    }
 
     [Fact]
-    public Task Search_Returns_All_System_Labels()
-    {
-        var dialogId = Guid.Empty;
-
-        return FlowBuilder.For(Application)
+    public Task Search_Returns_All_System_Labels() =>
+        FlowBuilder.For(Application)
             .CreateSimpleDialog(x => x.Dto.Party = IntegrationTestUser.DefaultParty)
             .SetSystemLabelsServiceOwner(x => x.AddLabels = [SystemLabel.Values.Archive, SystemLabel.Values.MarkedAsUnopened])
-            .SearchServiceOwnerDialogEndUserContexts((query, ctx) =>
+            .SearchServiceOwnerDialogEndUserContexts((query, ctx) => query.Party = [ctx.GetParty()])
+            .ExecuteAndAssert<PaginatedList<DialogEndUserContextItemDto>>((result, ctx) =>
             {
-                dialogId = ctx.GetDialogId();
-                query.Party = [ctx.GetParty()];
-            })
-            .ExecuteAndAssert<PaginatedList<DialogEndUserContextItemDto>>(result =>
-            {
-                var labels = result.Items.Single(item => item.DialogId == dialogId).SystemLabels;
+                var labels = result.Items.Single(item => item.DialogId == ctx.GetDialogId()).SystemLabels;
                 labels.Should().Contain(SystemLabel.Values.Archive);
                 labels.Should().Contain(SystemLabel.Values.MarkedAsUnopened);
                 labels.Distinct().Should().HaveCount(labels.Count);
             });
-    }
 }

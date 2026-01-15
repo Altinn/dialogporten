7ba26be9a6cd67e69d228bbdae8817bdf5fc5072
Amund Myrbostad <Lullinj98@gmail.com>
2026-01-15 08:56:27 +0100

    Added KeyedLock around UpdateIndex

--- src/Digdir.Domain.Dialogporten.Application/Features/V1/Common/Events/DialogSearch/DialogSearchIndexer.cs
+++ src/Digdir.Domain.Dialogporten.Application/Features/V1/Common/Events/DialogSearch/DialogSearchIndexer.cs
@@ -1,3 +1,4 @@
+using AsyncKeyedLock;
 using Digdir.Domain.Dialogporten.Application.Externals;
 using Digdir.Domain.Dialogporten.Domain.Dialogs.Events;
 using MediatR;
@@ -6,6 +7,8 @@ namespace Digdir.Domain.Dialogporten.Application.Features.V1.Common.Events.Dialo
 
 internal sealed class DialogSearchIndexer(IDialogSearchRepository db) : INotificationHandler<DialogCreatedDomainEvent>, INotificationHandler<DialogUpdatedDomainEvent>
 {
+    private static readonly AsyncKeyedLocker<string> Semaphore = new();
+    private const string IndexerLockKey = "dialog-search-indexer";
     private readonly IDialogSearchRepository _db = db ?? throw new ArgumentNullException(nameof(db));
 
     public Task Handle(DialogCreatedDomainEvent notification, CancellationToken cancellationToken) =>
@@ -14,6 +17,9 @@ internal sealed class DialogSearchIndexer(IDialogSearchRepository db) : INotific
     public Task Handle(DialogUpdatedDomainEvent notification, CancellationToken cancellationToken) =>
         UpdateIndex(notification.DialogId, cancellationToken);
 
-    private async Task UpdateIndex(Guid dialogId, CancellationToken cancellationToken) =>
+    private async Task UpdateIndex(Guid dialogId, CancellationToken cancellationToken)
+    {
+        using var _ = await Semaphore.LockAsync(IndexerLockKey, cancellationToken);
         await _db.UpsertFreeTextSearchIndex(dialogId, cancellationToken);
+    }
 }

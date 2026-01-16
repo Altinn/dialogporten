4f5644003d4b2d029ab2aed93d64ce456e607f59
Amund Myrbostad <Lullinj98@gmail.com>
2026-01-16 08:32:51 +0100

    Return empty conflicts

--- src/Digdir.Domain.Dialogporten.Application/Common/ReturnTypes/Conflict.cs
+++ src/Digdir.Domain.Dialogporten.Application/Common/ReturnTypes/Conflict.cs
@@ -4,5 +4,10 @@ namespace Digdir.Domain.Dialogporten.Application.Common.ReturnTypes;
 
 public sealed record Conflict(string PropertyName, string ErrorMessage)
 {
-    public List<ValidationFailure> ToValidationResults() => [new(PropertyName, ErrorMessage)];
+    public static Conflict Empty { get; } = new(string.Empty, string.Empty);
+
+    public List<ValidationFailure> ToValidationResults() =>
+        string.IsNullOrWhiteSpace(PropertyName) || string.IsNullOrWhiteSpace(ErrorMessage)
+            ? []
+            : [new(PropertyName, ErrorMessage)];
 }

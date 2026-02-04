80c13993bb9f30b23a201470d01995262c5c7348
Ole Jørgen Skogstad <skogstad@softis.net>
2026-02-04 19:34:09 +0100

    --wip-- [skip ci]

--- tests/Digdir.Domain.Dialogporten.Application.Integration.Tests/Features/V1/ServiceOwner/Transmissions/Commands/CreateTransmissionTests.cs
+++ tests/Digdir.Domain.Dialogporten.Application.Integration.Tests/Features/V1/ServiceOwner/Transmissions/Commands/CreateTransmissionTests.cs
@@ -1,25 +1,16 @@
 using System;
 using System.Threading.Tasks;
-using Digdir.Domain.Dialogporten.Application.Common.Authorization;
 using Digdir.Domain.Dialogporten.Application.Common.ReturnTypes;
-using Digdir.Domain.Dialogporten.Application.Features.V1.Common.Content;
-using Digdir.Domain.Dialogporten.Application.Features.V1.Common.Localizations;
 using Digdir.Domain.Dialogporten.Application.Features.V1.ServiceOwner.Dialogs.Commands.Create;
 using Digdir.Domain.Dialogporten.Application.Features.V1.ServiceOwner.Dialogs.Queries.Get;
 using Digdir.Domain.Dialogporten.Application.Integration.Tests.Common;
 using Digdir.Domain.Dialogporten.Application.Integration.Tests.Common.ApplicationFlow;
 using Digdir.Domain.Dialogporten.Application.Integration.Tests.Features.V1.Common;
 using Digdir.Domain.Dialogporten.Domain;
-using Digdir.Domain.Dialogporten.Domain.Dialogs.Entities.Transmissions;
 using Digdir.Tool.Dialogporten.GenerateFakeData;
 using AwesomeAssertions;
-using Digdir.Domain.Dialogporten.Application.Features.V1.ServiceOwner.Common.Actors;
 using Digdir.Domain.Dialogporten.Application.Features.V1.ServiceOwner.Dialogs.Commands.CreateTransmission;
 using Digdir.Library.Entity.Abstractions.Features.Identifiable;
-using Microsoft.Extensions.DependencyInjection;
-using static Digdir.Domain.Dialogporten.Application.Integration.Tests.Common.Common;
-using Constants = Digdir.Domain.Dialogporten.Domain.Common.Constants;
-using TransmissionDto = Digdir.Domain.Dialogporten.Application.Features.V1.ServiceOwner.Dialogs.Commands.Create.TransmissionDto;
 
 namespace Digdir.Domain.Dialogporten.Application.Integration.Tests.Features.V1.ServiceOwner.Transmissions.Commands;
 
@@ -42,37 +33,10 @@ public class CreateTransmissionTests : ApplicationCollectionFixture
                 command.Dto.Transmissions = [parentTransmission];
             })
             .AssertResult<CreateDialogSuccess>()
-            .SendCommand((_, ctx) =>
+            .CreateTransmission((transmission, ctx) =>
             {
                 var parentId = (Guid)ctx.Bag[ParentTransmissionIdKey]!;
-                var transmission = new CreateTransmissionDto
-                {
-                    Id = Guid.CreateVersion7(),
-                    CreatedAt = DateTimeOffset.UtcNow,
-                    Type = DialogTransmissionType.Values.Information,
-                    RelatedTransmissionId = parentId,
-                    Sender = new()
-                    {
-                        ActorType = Domain.Actors.ActorType.Values.ServiceOwner
-                    },
-                    Content = new()
-                    {
-                        Title = new ContentValueDto
-                        {
-                            Value = [new LocalizationDto
-                            {
-                                LanguageCode = "nb",
-                                Value = "Ny melding"
-                            }]
-                        }
-                    }
-                };
-
-                return new CreateTransmissionCommand
-                {
-                    DialogId = ctx.GetDialogId(),
-                    Transmissions = [transmission]
-                };
+                transmission.RelatedTransmissionId = parentId;
             })
             .AssertResult<CreateTransmissionSuccess>()
             .GetServiceOwnerDialog()

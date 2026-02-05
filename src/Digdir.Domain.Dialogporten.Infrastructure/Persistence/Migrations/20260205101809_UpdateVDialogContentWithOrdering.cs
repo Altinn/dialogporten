c1ce5e6dd590cd08332c5334027c72b75c414eaf
Magnus Sandgren <5285192+MagnusSandgren@users.noreply.github.com>
2026-02-05 11:21:34 +0100

    fix(dialogSearch): add ordering to VDialogContent view for improved search consistency

---
+++ src/Digdir.Domain.Dialogporten.Infrastructure/Persistence/Migrations/20260205101809_UpdateVDialogContentWithOrdering.cs
@@ -0,0 +1,40 @@
+using Digdir.Domain.Dialogporten.Infrastructure.Persistence.Sql;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Digdir.Domain.Dialogporten.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateVDialogContentWithOrdering : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var scripts = new[]
+            {
+                "Dialog/Search/View.VDialogContent.V3.sql"
+            };
+
+            foreach (var sql in MigrationSqlLoader.LoadAll(scripts))
+            {
+                migrationBuilder.Sql(sql);
+            }
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            var scripts = new[]
+            {
+                "Dialog/Search/View.VDialogContent.V2.sql"
+            };
+
+            foreach (var sql in MigrationSqlLoader.LoadAll(scripts))
+            {
+                migrationBuilder.Sql(sql);
+            }
+
+        }
+    }
+}

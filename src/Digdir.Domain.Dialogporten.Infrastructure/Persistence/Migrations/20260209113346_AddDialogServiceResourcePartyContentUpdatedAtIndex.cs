356ed0fecb53120ac16ee495db28f0e9d271bd96
Bjørn Dybvik Langfors <bdl@digdir.no>
2026-02-09 12:36:06 +0100

    Add new service driven index

---
+++ src/Digdir.Domain.Dialogporten.Infrastructure/Persistence/Migrations/20260209113346_AddDialogServiceResourcePartyContentUpdatedAtIndex.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Digdir.Domain.Dialogporten.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDialogServiceResourcePartyContentUpdatedAtIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Dialog_ServiceResource_Party_ContentUpdatedAt_Id_NotDeleted",
+                table: "Dialog",
+                columns: new[] { "ServiceResource", "Party", "ContentUpdatedAt", "Id" },
+                descending: new[] { false, false, true, true },
+                filter: "\"Deleted\" = false");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Dialog_ServiceResource_Party_ContentUpdatedAt_Id_NotDeleted",
+                table: "Dialog");
+        }
+    }
+}

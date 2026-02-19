781fbddc10ee32715a6dfd1f0110ca4bdd05ae72
Ole Jørgen Skogstad <skogstad@softis.net>
2026-02-19 10:44:01 +0100

    use seed

--- tests/Digdir.Domain.Dialogporten.Application.Integration.Tests/Features/V1/ServiceOwner/Dialogs/Queries/Get/GetDialogSnapshotTests.cs
+++ tests/Digdir.Domain.Dialogporten.Application.Integration.Tests/Features/V1/ServiceOwner/Dialogs/Queries/Get/GetDialogSnapshotTests.cs
@@ -7,10 +7,12 @@ namespace Digdir.Domain.Dialogporten.Application.Integration.Tests.Features.V1.S
 [Collection(nameof(DialogCqrsCollectionFixture))]
 public class GetDialogSnapshotTests(DialogApplication application) : ApplicationCollectionFixture(application)
 {
+    private readonly Guid _seed = Guid.Parse("915d005a-efd1-4ede-886a-f5889a0d3202");
+
     [Fact]
     public Task Get_ServiceOwner_Dialog_Snapshot_Test() =>
         FlowBuilder.For(Application)
-            .CreateComplexDialog()
+            .CreateComplexDialog(seed: _seed.GetHashCode())
             .GetServiceOwnerDialog()
             .VerifySnapshot()
             .ExecuteAndAssert<DialogDto>();

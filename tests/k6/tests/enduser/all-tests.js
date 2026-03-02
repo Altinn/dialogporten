cfed83a61b3d9814051a2ace19996954c4cecaf2
Ole Jørgen Skogstad <skogstad@softis.net>
2026-03-02 12:55:17 +0100

    test: re-build all-tests.js

--- tests/k6/tests/enduser/all-tests.js
+++ tests/k6/tests/enduser/all-tests.js
@@ -1,7 +1,6 @@
 // This file is generated, see "scripts" directory
 import { default as DialogGetAcceptLanguage } from './DialogGetAcceptLanguage.js';
 import { default as dialogBulkSystemLabels } from './dialogBulkSystemLabels.js';
-import { default as dialogDetails } from './dialogDetails.js';
 import { default as dialogSearch } from './dialogSearch.js';
 import { default as dialogSystemLabelLog } from './dialogSystemLabelLog.js';
 import { default as dialogSystemUser } from './dialogSystemUser.js';
@@ -11,7 +10,6 @@ import { default as parties } from './parties.js';
 export default function() {
   DialogGetAcceptLanguage();
   dialogBulkSystemLabels();
-  dialogDetails();
   dialogSearch();
   dialogSystemLabelLog();
   dialogSystemUser();

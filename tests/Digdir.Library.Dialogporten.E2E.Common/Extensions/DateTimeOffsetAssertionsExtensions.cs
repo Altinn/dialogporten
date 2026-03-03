806f3e6129c3c4e397acaff5a2c37bc7b974e4b2
Ole Jørgen Skogstad <skogstad@softis.net>
2026-03-03 09:23:36 +0100

    fix name

--- tests/Digdir.Library.Dialogporten.E2E.Common/Extensions/DateTimeOffsetAssertionsExtensions.cs
+++ tests/Digdir.Library.Dialogporten.E2E.Common/Extensions/DateTimeOffsetAssertionsExtensions.cs
@@ -6,7 +6,7 @@ public static class DateTimeOffsetAssertionsExtensions
 {
     extension(DateTimeOffset dateTime)
     {
-        public void BeCloseToWithinSecond(DateTimeOffset expected) =>
+        public void BeCloseToWithinSecondOf(DateTimeOffset expected) =>
             dateTime.Should().BeCloseTo(expected, TimeSpan.FromSeconds(1));
     }
 }

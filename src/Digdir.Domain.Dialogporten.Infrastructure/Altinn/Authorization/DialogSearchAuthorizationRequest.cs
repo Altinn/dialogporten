8963cceadf5e98bb08c8ef6e607e845746d92504
Ole Jørgen Skogstad <skogstad@softis.net>
2026-03-19 19:45:49 +0100

    trivial: remove unused class DialogSearchAuthorizationRequestExtensions

--- src/Digdir.Domain.Dialogporten.Infrastructure/Altinn/Authorization/DialogSearchAuthorizationRequest.cs
+++ src/Digdir.Domain.Dialogporten.Infrastructure/Altinn/Authorization/DialogSearchAuthorizationRequest.cs
@@ -1,7 +1,4 @@
 ﻿using System.Security.Claims;
-using System.Security.Cryptography;
-using System.Text;
-using Digdir.Domain.Dialogporten.Application.Common.Extensions;
 
 namespace Digdir.Domain.Dialogporten.Infrastructure.Altinn.Authorization;
 
@@ -10,23 +7,4 @@ internal sealed class DialogSearchAuthorizationRequest
     public required List<Claim> Claims { get; init; }
     public List<string> ConstraintParties { get; set; } = [];
     public List<string> ConstraintServiceResources { get; set; } = [];
-}
-
-internal static class DialogSearchAuthorizationRequestExtensions
-{
-    public static string GenerateCacheKey(this DialogSearchAuthorizationRequest request)
-    {
-        var claimsKey = string.Join(";", request.Claims.GetIdentifyingClaims()
-            .Select(c => $"{c.Type}:{c.Value}"));
-
-        var partiesKey = string.Join(";", request.ConstraintParties.OrderBy(p => p));
-        var resourcesKey = string.Join(";", request.ConstraintServiceResources.OrderBy(r => r));
-
-        var rawKey = $"{claimsKey}|{partiesKey}|{resourcesKey}";
-
-        var hashBytes = SHA256.HashData(Encoding.UTF8.GetBytes(rawKey));
-        var hashString = Convert.ToHexStringLower(hashBytes);
-
-        return $"auth:search:{hashString}";
-    }
 }

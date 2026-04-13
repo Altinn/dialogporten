6a2a46c79e30009b090cc2662fde861dca826ab5
Ole Jørgen Skogstad <skogstad@softis.net>
2026-04-13 14:48:36 +0200

    wip

--- src/Digdir.Domain.Dialogporten.WebApi/OpenApiServiceCollectionExtensions.cs
+++ src/Digdir.Domain.Dialogporten.WebApi/OpenApiServiceCollectionExtensions.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using Digdir.Domain.Dialogporten.WebApi.Common;
 using FastEndpoints;
 using Microsoft.AspNetCore.OpenApi;
@@ -6,8 +7,13 @@ namespace Digdir.Domain.Dialogporten.WebApi;
 
 internal static class OpenApiServiceCollectionExtensions
 {
-    internal static IServiceCollection AddOpenApi(this IServiceCollection services, string apiVersion, string audience) =>
-        services.AddOpenApi($"{apiVersion}.{audience}", options =>
+    private static readonly HashSet<string> AudiencesValidated = [];
+
+    internal static IServiceCollection AddOpenApi(this IServiceCollection services, string apiVersion, string audience)
+    {
+        EnsureUniqueOperationIds();
+
+        return services.AddOpenApi($"{apiVersion}.{audience}", options =>
         {
             options.AddDocumentTransformer((doc, _, _) =>
             {
@@ -33,6 +39,7 @@ internal static class OpenApiServiceCollectionExtensions
                 return Task.CompletedTask;
             });
         });
+    }
 
     private static string? GetEndpointName(this OpenApiOperationTransformerContext context) =>
         context.Description
@@ -43,4 +50,27 @@ internal static class OpenApiServiceCollectionExtensions
             .EndpointType?
             .FullName?
             .Split(".")[^1];
+
+    private static void EnsureUniqueOperationIds()
+    {
+        if (!AudiencesValidated.Add("all"))
+            return;
+
+        var endpointTypes = WebApiAssemblyMarker.Assembly.GetTypes()
+            .Where(type => type is { IsClass: true, IsAbstract: false })
+            .Where(type => type.GetCustomAttribute<OpenApiOperationIdAttribute>() is not null)
+            .ToList();
+
+        var duplicates = endpointTypes
+            .GroupBy(type => type.GetCustomAttribute<OpenApiOperationIdAttribute>()!.OperationId)
+            .Where(group => group.Count() > 1)
+            .ToList();
+
+        if (duplicates.Count == 0)
+            return;
+
+        var details = string.Join(", ",
+            duplicates.Select(duplicate => $"'{duplicate.Key}' on [{string.Join(", ", duplicate.Select(type => type.Name))}]"));
+        throw new InvalidOperationException($"Duplicate [OpenApiOperationId] values detected: {details}");
+    }
 }

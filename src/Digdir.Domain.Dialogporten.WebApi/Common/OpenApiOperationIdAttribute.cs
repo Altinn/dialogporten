e52777187aa7bf47fab78fe2abb9515185496839
Ole Jørgen Skogstad <skogstad@softis.net>
2026-04-16 08:48:41 +0200

    simpl

--- src/Digdir.Domain.Dialogporten.WebApi/Common/OpenApiOperationIdAttribute.cs
+++ src/Digdir.Domain.Dialogporten.WebApi/Common/OpenApiOperationIdAttribute.cs
@@ -1,12 +1,7 @@
 namespace Digdir.Domain.Dialogporten.WebApi.Common;
 
 [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
-public sealed class OpenApiOperationIdAttribute : Attribute
+public sealed class OpenApiOperationIdAttribute(string operationId) : Attribute
 {
-    public string OperationId { get; }
-
-    public OpenApiOperationIdAttribute(string operationId)
-    {
-        OperationId = operationId;
-    }
+    public string OperationId { get; } = operationId;
 }

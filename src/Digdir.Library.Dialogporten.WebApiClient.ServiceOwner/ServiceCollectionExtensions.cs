0f775f4cd019c7756987eae9582b085aec59521c
Ole Jørgen Skogstad <skogstad@softis.net>
2026-04-21 14:04:53 +0200

    cleanup service registration

--- src/Digdir.Library.Dialogporten.WebApiClient.ServiceOwner/ServiceCollectionExtensions.cs
+++ src/Digdir.Library.Dialogporten.WebApiClient.ServiceOwner/ServiceCollectionExtensions.cs
@@ -1,17 +1,4 @@
-using System.CodeDom.Compiler;
-using System.Reflection;
-using System.Text.Json;
-using System.Text.Json.Serialization;
-using Altinn.ApiClients.Dialogporten;
-using Altinn.ApiClients.Dialogporten.Common;
-using Altinn.ApiClients.Dialogporten.Infrastructure;
-using Altinn.ApiClients.Dialogporten.Services;
-using Altinn.ApiClients.Maskinporten.Extensions;
-using Altinn.ApiClients.Maskinporten.Services;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.DependencyInjection.Extensions;
-using Microsoft.Extensions.Options;
-using Refit;
 
 namespace Altinn.ApiClients.Dialogporten.ServiceOwner;
 
@@ -20,52 +7,7 @@ public static class ServiceCollectionExtensions
     private const string ClientDefinitionKey = "dialogporten-sp-sdk-serviceowner";
 
     public static IServiceCollection AddDialogportenClient(this IServiceCollection services, DialogportenSettings settings)
-    {
-        if (!DialogportenSettings.Validate())
-        {
-            throw new InvalidOperationException("Invalid configuration");
-        }
-        services.TryAddSingleton<IOptions<DialogportenSettings>>(new OptionsWrapper<DialogportenSettings>(settings));
-
-        services.RegisterMaskinportenClientDefinition<SettingsJwkClientDefinition>(ClientDefinitionKey, settings.Maskinporten);
-
-        var refitClients = AssemblyMarker.Assembly.GetTypes()
-            .Where(x =>
-                x.IsInterface &&
-                x.GetCustomAttribute<GeneratedCodeAttribute>()?.Tool == "Refitter")
-            .ToList();
-
-        var jsonSerializerOptions = new JsonSerializerOptions
-        {
-            DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
-            Converters = { new JsonStringEnumConverter() }
-        };
-
-        foreach (var refitClient in refitClients)
-        {
-            services
-                .AddRefitClient(refitClient, new RefitSettings
-                {
-                    ContentSerializer = new SystemTextJsonContentSerializer(jsonSerializerOptions)
-                })
-                .ConfigureHttpClient(c => c.BaseAddress = new Uri(settings.BaseUri))
-                .AddMaskinportenHttpMessageHandler<SettingsJwkClientDefinition>(ClientDefinitionKey);
-        }
-
-        services
-            .AddRefitClient<IInternalDialogportenApi>()
-            .ConfigureHttpClient(c => c.BaseAddress = new Uri(settings.BaseUri));
-
-        services.AddHostedService<EdDsaSecurityKeysCacheService>();
-        services.TryAddTransient<IDialogTokenValidator, DialogTokenValidator>();
-        services.TryAddSingleton<DefaultEdDsaSecurityKeysCache>();
-        services.TryAddSingleton<IEdDsaSecurityKeysCache>(x => x.GetRequiredService<DefaultEdDsaSecurityKeysCache>());
-        services.TryAddTransient<IClock, DefaultClock>();
-
-        services.TryAddTransient<IServiceOwnerApi, ServiceOwnerApi>();
-
-        return services;
-    }
+        => services.AddDialogportenClientCore<IServiceOwnerApi, ServiceOwnerApi>(settings, ClientDefinitionKey, AssemblyMarker.Assembly);
 
     public static IServiceCollection AddDialogportenClient(this IServiceCollection services, Action<DialogportenSettings> configureOptions)
     {

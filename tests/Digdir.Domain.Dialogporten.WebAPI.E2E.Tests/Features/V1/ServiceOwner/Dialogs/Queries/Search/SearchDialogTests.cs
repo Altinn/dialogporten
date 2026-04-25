015c55511e93a463911519e77ef5425b299e8f14
Ole Jørgen Skogstad <skogstad@softis.net>
2026-04-25 22:13:05 +0200

    theory

--- tests/Digdir.Domain.Dialogporten.WebAPI.E2E.Tests/Features/V1/ServiceOwner/Dialogs/Queries/Search/SearchDialogTests.cs
+++ tests/Digdir.Domain.Dialogporten.WebAPI.E2E.Tests/Features/V1/ServiceOwner/Dialogs/Queries/Search/SearchDialogTests.cs
@@ -9,8 +9,10 @@ namespace Digdir.Domain.Dialogporten.WebAPI.E2E.Tests.Features.V1.ServiceOwner.D
 public class SearchDialogTests(WebApiE2EFixture fixture) : E2ETestBase<WebApiE2EFixture>(fixture)
 {
 
-    [E2EFact]
-    public async Task Should_Filter_By_ServiceResource_When_EndUserId_Is_Specified()
+    [E2ETheory]
+    [InlineData(true)]
+    [InlineData(false)]
+    public async Task Should_Filter_By_ServiceResource(bool withEndUserId)
     {
         // Arrange
         var sentinelLabel = Guid.NewGuid().ToString();
@@ -28,45 +30,7 @@ public class SearchDialogTests(WebApiE2EFixture fixture) : E2ETestBase<WebApiE2E
             {
                 ServiceOwnerLabels = [sentinelLabel],
                 ServiceResource = [E2EConstants.AlternateServiceResource],
-                EndUserId = E2EConstants.DefaultParty
-            }, ct),
-            isSuccessful: response =>
-                response.Content is { Items.Count: 1 } content
-                && content.Items.Single().Id == dialogId
-                && content.Items.All(x => x.Id != controlDialogId),
-            degradationMessage: "Search indexing speed is degraded.");
-
-        // Assert
-        searchResult.ShouldHaveStatusCode(HttpStatusCode.OK);
-        searchResult.Content.Should().NotBeNull();
-
-        var content = searchResult.Content;
-        content.Items.Should().ContainSingle();
-        content.Items.Single().Id.Should().Be(dialogId);
-        content.Items.Single().ServiceResource.Should()
-            .Be(E2EConstants.AlternateServiceResource);
-    }
-
-    [E2EFact]
-    public async Task Should_Filter_By_ServiceResource()
-    {
-        // Arrange
-        var sentinelLabel = Guid.NewGuid().ToString();
-
-        var dialogId = await Fixture.ServiceownerApi.CreateSearchDialogAsync(sentinelLabel, dialog =>
-        {
-            dialog.ServiceResource = E2EConstants.AlternateServiceResource;
-        });
-
-        var controlDialogId = await Fixture.ServiceownerApi.CreateSearchDialogAsync(sentinelLabel);
-
-        // Act
-        var searchResult = await E2ERetryPolicies.RetryUntilAsync(
-            ct => Fixture.ServiceownerApi.V1ServiceOwnerDialogsQueriesSearchDialog(new()
-            {
-                ServiceOwnerLabels = [sentinelLabel],
-                ServiceResource = [E2EConstants.AlternateServiceResource],
-                EndUserId = null!
+                EndUserId = withEndUserId ? E2EConstants.DefaultParty : null!
             }, ct),
             isSuccessful: response =>
                 response.Content is { Items.Count: 1 } content

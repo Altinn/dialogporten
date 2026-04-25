415fa31e42d030546d617cd3d06db45ce7b22f45
Ole Jørgen Skogstad <skogstad@softis.net>
2026-04-25 20:07:27 +0200

    wip

---
+++ tests/Digdir.Domain.Dialogporten.WebAPI.E2E.Tests/Features/V1/ServiceOwner/Dialogs/Queries/Search/SearchDialogTests.cs
@@ -0,0 +1,49 @@
+using System.Net;
+using AwesomeAssertions;
+using Digdir.Library.Dialogporten.E2E.Common;
+using Digdir.Library.Dialogporten.E2E.Common.Extensions;
+
+namespace Digdir.Domain.Dialogporten.WebAPI.E2E.Tests.Features.V1.ServiceOwner.Dialogs.Queries.Search;
+
+[Collection(nameof(WebApiTestCollectionFixture))]
+public class SearchDialogTests(WebApiE2EFixture fixture) : E2ETestBase<WebApiE2EFixture>(fixture)
+{
+
+    [E2EFact]
+    public async Task Should_Filter_By_ServiceResource_When_EndUserId_Is_Specified()
+    {
+        // Arrange
+        var sentinelLabel = Guid.NewGuid().ToString();
+
+        var dialogId = await Fixture.ServiceownerApi.CreateSearchDialogAsync(sentinelLabel, dialog =>
+        {
+            dialog.ServiceResource = E2EConstants.AlternateServiceResource;
+        });
+
+        var controlDialogId = await Fixture.ServiceownerApi.CreateSearchDialogAsync(sentinelLabel);
+
+        // Act
+        var searchResult = await E2ERetryPolicies.RetryUntilAsync(
+            ct => Fixture.ServiceownerApi.V1ServiceOwnerDialogsQueriesSearchDialog(new()
+            {
+                ServiceOwnerLabels = [sentinelLabel],
+                ServiceResource = [E2EConstants.AlternateServiceResource],
+                EndUserId = E2EConstants.DefaultParty
+            }, ct),
+            isSuccessful: response =>
+                response.Content is { Items.Count: 1 } content
+                && content.Items.Single().Id == dialogId
+                && content.Items.All(x => x.Id != controlDialogId),
+            degradationMessage: "Search indexing speed is degraded.");
+
+        // Assert
+        searchResult.ShouldHaveStatusCode(HttpStatusCode.OK);
+        searchResult.Content.Should().NotBeNull();
+
+        var content = searchResult.Content;
+        content.Items.Should().ContainSingle();
+        content.Items.Single().Id.Should().Be(dialogId);
+        content.Items.Single().ServiceResource.Should()
+            .Be(E2EConstants.AlternateServiceResource);
+    }
+}

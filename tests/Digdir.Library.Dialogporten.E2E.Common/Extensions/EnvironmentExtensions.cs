e907476e84f5369238052303d7eab5e58a05a655
Ole Jørgen Skogstad <skogstad@softis.net>
2026-04-27 14:04:49 +0200

    wip

--- tests/Digdir.Library.Dialogporten.E2E.Common/Extensions/EnvironmentExtensions.cs
+++ tests/Digdir.Library.Dialogporten.E2E.Common/Extensions/EnvironmentExtensions.cs
@@ -6,7 +6,7 @@ public static class EnvironmentExtensions
 {
     extension(Environment)
     {
-        public static string GetDotnetEnvironment() => "staging";
+        public static string GetDotnetEnvironment() => "Development";
 
         public static string GetTokenGeneratorEnvironment() =>
             Environment.GetDotnetEnvironment() switch

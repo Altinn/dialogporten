2a9056e278d1ddc1ff5b068c95d034506be0d6a6
Ole Jørgen Skogstad <skogstad@softis.net>
2026-05-06 16:23:28 +0200

    rename refactor

--- tests/Digdir.Domain.Dialogporten.GraphQl.Unit.Tests/DialogportenHttpRequestInterceptorTests.cs
+++ tests/Digdir.Domain.Dialogporten.GraphQl.Unit.Tests/DialogportenHttpRequestInterceptorTests.cs
@@ -7,14 +7,14 @@ using Microsoft.AspNetCore.Http;
 
 namespace Digdir.Domain.Dialogporten.GraphQl.Unit.Tests;
 
-public class UserTypeAndAcceptLanguageInterceptorTests
+public class DialogportenHttpRequestInterceptorTests
 {
     private const string AuthenticationType = "Bearer";
 
     [Fact]
     public async Task Authenticated_user_with_unknown_user_type_throws_graphql_exception()
     {
-        var interceptor = new UserTypeAndAcceptLanguageInterceptor();
+        var interceptor = new DialogportenHttpRequestInterceptor();
         var context = new DefaultHttpContext
         {
             User = new ClaimsPrincipal(new ClaimsIdentity(claims: [], AuthenticationType))
@@ -32,7 +32,7 @@ public class UserTypeAndAcceptLanguageInterceptorTests
     [Fact]
     public async Task Authenticated_user_with_known_user_type_passes_through()
     {
-        var interceptor = new UserTypeAndAcceptLanguageInterceptor();
+        var interceptor = new DialogportenHttpRequestInterceptor();
         var context = new DefaultHttpContext
         {
             User = new ClaimsPrincipal(new ClaimsIdentity(

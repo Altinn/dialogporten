d385a67af385ce531d362116c037c2145486fc49
Ole Jørgen Skogstad <skogstad@softis.net>
2026-05-10 16:19:42 +0200

    trivial: remove automapper serviceowner get transmission (#3918)
    <!--- Provide a general summary of your changes in the Title above -->
    
    ## Description
    
    <!--- Describe your changes in detail -->
    
    ## Related Issue(s)
    
    - #967 
    
    ## Verification
    
    - [ ] **Your** code builds clean without any errors or warnings
    - [ ] Manual testing done (required)
    - [ ] Relevant automated test added 
    - [ ] Database changes manually applied to prod/YT01 (if relevant)
    
    ## Documentation
    
    - [ ] Documentation is updated (either in `docs`-directory, Altinnpedia
    or a separate linked PR in
    [altinn-studio-docs.](https://github.com/Altinn/altinn-studio-docs), if
    applicable)

--- src/Digdir.Library.Dialogporten.WebApiClient.ServiceOwner/Features/V1/RefitterInterface.cs
+++ src/Digdir.Library.Dialogporten.WebApiClient.ServiceOwner/Features/V1/RefitterInterface.cs
@@ -2717,12 +2717,6 @@ namespace Altinn.ApiClients.Dialogporten.ServiceOwner.Features.V1
         public System.Guid? RelatedTransmissionId { get; set; }
 
         /// <summary>
-        /// The date and time when the transmission was deleted, if applicable.
-        /// </summary>
-        [JsonPropertyName("deletedAt")]
-        public System.DateTimeOffset? DeletedAt { get; set; }
-
-        /// <summary>
         /// The type of the transmission.
         /// </summary>
         [JsonPropertyName("type")]

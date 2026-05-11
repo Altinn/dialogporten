44b323675516987f5c9766a135f13dcc40f962a1
Ole Jørgen Skogstad <skogstad@softis.net>
2026-05-11 21:02:26 +0200

    remove deletedat

--- src/Digdir.Library.Dialogporten.WebApiClient.ServiceOwner/Features/V1/RefitterInterface.cs
+++ src/Digdir.Library.Dialogporten.WebApiClient.ServiceOwner/Features/V1/RefitterInterface.cs
@@ -1758,12 +1758,6 @@ namespace Altinn.ApiClients.Dialogporten.ServiceOwner.Features.V1
         public System.Guid? RelatedTransmissionId { get; set; }
 
         /// <summary>
-        /// The date and time when the transmission was deleted, if applicable.
-        /// </summary>
-        [JsonPropertyName("deletedAt")]
-        public System.DateTimeOffset? DeletedAt { get; set; }
-
-        /// <summary>
         /// The type of the transmission.
         /// </summary>
         [JsonPropertyName("type")]

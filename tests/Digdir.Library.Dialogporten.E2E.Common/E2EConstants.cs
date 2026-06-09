36279b37081b6d1e60f7848a8b1a17dff090aeda
Mats Jørgensen <mats.jorgensen@digdir.no>
2026-06-09 12:57:57 +0200

    Allow for different system user IDs by env

--- tests/Digdir.Library.Dialogporten.E2E.Common/E2EConstants.cs
+++ tests/Digdir.Library.Dialogporten.E2E.Common/E2EConstants.cs
@@ -14,16 +14,37 @@ public static class E2EConstants
     /// <summary>
     /// Default System user has the following properties:
     /// - Access to AccessPackage "ordinaer-post-til-virksomheten" on <see cref="DefaultServiceResource"/>
+    /// - Daglig leder: 29886896598
     /// </summary>
-    public const string DefaultSystemUserId = "51551dd0-b9c5-4f4b-a4a2-9b7e3669b364";
+    public static readonly Func<TokenGeneratorEnvironment, string> DefaultSystemUserId = env =>
+    {
+        return env switch
+        {
+            TokenGeneratorEnvironment.At23 => "51551dd0-b9c5-4f4b-a4a2-9b7e3669b364",
+            TokenGeneratorEnvironment.Tt02 => "7633be4c-dbd5-49f4-bbc4-c058b275de7b",
+            TokenGeneratorEnvironment.Yt01 => throw new ArgumentException("No system user in YT"),
+            _ => throw new ArgumentException($"System user doesnt exist in env {env}")
+        };
+    };
+
     public const string DefaultSystemUserOrgNo = "310057223";
     public const string DefaultSystemUserOrgUrn = "urn:altinn:organization:identifier-no:" + DefaultSystemUserOrgNo;
 
     /// <summary>
     /// Alternate System user has the following properties:
     /// - Direct access to resource <see cref="AlternateServiceResource"/>
+    /// - Daglig leder: 15864799741
     /// </summary>
-    public const string AlternateSystemUserId = "54cb69df-3d8a-4432-804e-742e40de6211";
+    public static readonly Func<TokenGeneratorEnvironment, string> AlternateSystemUserId = env =>
+    {
+        return env switch
+        {
+            TokenGeneratorEnvironment.At23 => "54cb69df-3d8a-4432-804e-742e40de6211",
+            TokenGeneratorEnvironment.Tt02 => "96a46590-1c2a-4e46-a7e7-975f40b985ef",
+            TokenGeneratorEnvironment.Yt01 => throw new ArgumentException("No system user in YT"),
+            _ => throw new ArgumentException($"System user doesnt exist in env {env}")
+        };
+    };
     public const string AlternateSystemUserOrgNo = "313006425";
     public const string AlternateSystemUserOrgUrn = "urn:altinn:organization:identifier-no:" + AlternateSystemUserOrgNo;
 

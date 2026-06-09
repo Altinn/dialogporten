5e9d80e0236d77a1d394e476c9b2ae54a9e5805a
Bjørn Dybvik Langfors <bdl@digdir.no>
2026-06-09 10:54:52 +0200

    fix: email party mapping, remove legacy/feide authentication (#4085)

--- src/Digdir.Domain.Dialogporten.Application/ApplicationSettings.cs
+++ src/Digdir.Domain.Dialogporten.Application/ApplicationSettings.cs
@@ -19,9 +19,6 @@ public sealed class FeatureToggle
     public bool UseAltinnAutoAuthorizedPartiesQueryParameters { get; init; }
     public bool EnablePartyFiltersForSystemUsers { get; init; }
     public bool UseCorrectPersonNameOrdering { get; init; }
-    public bool UseAccessManagementForAltinnSelfIdentifiedUsers { get; init; }
-    public bool UseAccessManagementForIdportenEmailUsers { get; init; }
-    public bool UseAccessManagementForFeideUsers { get; init; }
 }
 
 public enum BadDataHandling

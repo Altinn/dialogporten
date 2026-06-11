c7e0e515cdc4822bcd9357e3ed3cb8ae090c5e50
Mats Jørgensen <majoer@users.noreply.github.com>
2026-06-11 09:28:36 +0200

    fix: Add back domain events for seen/updated on get dialog (#3975)
    * Add back domain events for seen/updated on get dialog
    
    * Use ConcurrentQueue for thread safety
    
    * Use raw sql for marking dialog as seen

--- src/Digdir.Domain.Dialogporten.Infrastructure/Persistence/Repositories/DialogSeenLogWriter.cs
+++ src/Digdir.Domain.Dialogporten.Infrastructure/Persistence/Repositories/DialogSeenLogWriter.cs
@@ -75,17 +75,29 @@ internal sealed class DialogSeenLogWriter(
             }
         );
 
-        var rowsUpdated = await db.Dialogs
-            .Where(x => x.Id == dialog.Id && x.ContentUpdatedAt == dialog.ContentUpdatedAt)
-            .ExecuteUpdateAsync(s => s.SetProperty(d => d.IsSeenSinceLastContentUpdate, true), cancellationToken);
-        var newIsSeenSinceLastContentUpdate = rowsUpdated > 0 || dialog.IsSeenSinceLastContentUpdate;
+        var newSeenLog = seenLogWriteResult.NewDialogSeenLog;
+        var isSeenSinceLastContentUpdateSetToTrue = (await MarkThisRevisionAsSeen(dialog, cancellationToken)) > 0;
 
-        return new DialogSeenResult(seenLogWriteResult.DialogSeenLog, newIsSeenSinceLastContentUpdate);
+        return new DialogSeenResult(
+            NewSeenLog: newSeenLog,
+            CausedChangesOutsideEf: isSeenSinceLastContentUpdateSetToTrue || newSeenLog is not null,
+            IsContentSeen: isSeenSinceLastContentUpdateSetToTrue || dialog.IsSeenSinceLastContentUpdate
+        );
+    }
+
+    private Task<int> MarkThisRevisionAsSeen(DialogEntity dialog, CancellationToken cancellationToken)
+    {
+        return db.Database
+            .ExecuteSqlInterpolatedAsync($"""
+                                          UPDATE "Dialog" 
+                                          SET "IsSeenSinceLastContentUpdate"=true
+                                          WHERE "Id"={dialog.Id}
+                                            AND "ContentUpdatedAt"={dialog.ContentUpdatedAt}
+                                          """, cancellationToken);
     }
 
     private sealed record EnsureSeenLogResult(
-        Guid ActorNameId,
-        DialogSeenLog? DialogSeenLog
+        DialogSeenLog? NewDialogSeenLog
     );
 
     private async Task<EnsureSeenLogResult> EnsureSeenLog(
@@ -111,21 +123,22 @@ internal sealed class DialogSeenLogWriter(
 
         return new EnsureSeenLogResult
         (
-            actorNameId,
-            rowsUpdated == 0 ? null : new DialogSeenLog
-            {
-                Id = seenLogId,
-                CreatedAt = seenAt,
-                SeenBy = new DialogSeenLogSeenByActor
+            rowsUpdated == 0
+                ? null
+                : new DialogSeenLog
                 {
-                    ActorNameEntity = new ActorName
+                    Id = seenLogId,
+                    CreatedAt = seenAt,
+                    SeenBy = new DialogSeenLogSeenByActor
                     {
-                        ActorId = normalizedActorId,
-                        Name = actorName
-                    },
-                    ActorTypeId = actorType
+                        ActorNameEntity = new ActorName
+                        {
+                            ActorId = normalizedActorId,
+                            Name = actorName
+                        },
+                        ActorTypeId = actorType
+                    }
                 }
-            }
         );
     }
 

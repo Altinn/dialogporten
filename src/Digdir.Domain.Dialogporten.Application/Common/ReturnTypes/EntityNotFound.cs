b577535e3d1bcc3b843bea7568e506014d57a813
Ole Jørgen Skogstad <skogstad@softis.net>
2026-06-12 14:54:01 +0200

    cleanup

--- src/Digdir.Domain.Dialogporten.Application/Common/ReturnTypes/EntityNotFound.cs
+++ src/Digdir.Domain.Dialogporten.Application/Common/ReturnTypes/EntityNotFound.cs
@@ -1,5 +1,4 @@
-﻿using Altinn.Authorization.ProblemDetails;
-using FluentValidation.Results;
+﻿using FluentValidation.Results;
 
 namespace Digdir.Domain.Dialogporten.Application.Common.ReturnTypes;
 
@@ -9,16 +8,11 @@ public sealed record EntityNotFound<T>(IEnumerable<object> Keys) : EntityNotFoun
     public EntityNotFound(Guid key) : this(new object[] { key }) { }
 }
 
-public record EntityNotFound(string Name, IEnumerable<object> Keys) //: ApplicationReturnBase
+public record EntityNotFound(string Name, IEnumerable<object> Keys)
 {
     public string Message => $"Entity '{Name}' with the following key(s) was not found: ({string.Join(", ", Keys)}).";
     public EntityNotFound(string name, IEnumerable<Guid> keys) : this(name, keys.Cast<object>()) { }
     public EntityNotFound(string name, Guid key) : this(name, new object[] { key }) { }
 
     public List<ValidationFailure> ToValidationResults() => [new(Name, Message)];
-}
-
-public record ApplicationReturnBase
-{
-    public required ProblemInstance ProblemInstance { get; set; }
 }

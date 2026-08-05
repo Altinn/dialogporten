3e2f09eef3b4ee7de3b90f583841ffba66b36c1b
Mats Jørgensen <mats.jorgensen@digdir.no>
2026-08-05 10:11:35 +0200

    Make class static

--- src/Digdir.Domain.Dialogporten.Domain/Common/ServiceResourceUrnFactory.cs
+++ src/Digdir.Domain.Dialogporten.Domain/Common/ServiceResourceUrnFactory.cs
@@ -1,6 +1,6 @@
 namespace Digdir.Domain.Dialogporten.Domain.Common;
 
-public sealed class ServiceResourceUrnFactory
+public static class ServiceResourceUrnFactory
 {
     public static string CreateUrn(string resourceId) => $"{Constants.ServiceResourcePrefix}{resourceId}";
 }

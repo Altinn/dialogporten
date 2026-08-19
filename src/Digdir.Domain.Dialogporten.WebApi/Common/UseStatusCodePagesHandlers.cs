23c90d1246fec2bf645adb2d17766832ebbd7d88
Mats Jørgensen <mats.jorgensen@digdir.no>
2026-08-19 12:40:22 +0200

    Refactor to make meaning clear

--- src/Digdir.Domain.Dialogporten.WebApi/Common/UseStatusCodePagesHandlers.cs
+++ src/Digdir.Domain.Dialogporten.WebApi/Common/UseStatusCodePagesHandlers.cs
@@ -17,26 +17,15 @@ public static class UseStatusCodePagesHandlers
         var context = statusCodeContext.HttpContext;
         var status = context.Response.StatusCode;
 
-        if (status == (int)HttpStatusCode.NotFound)
-        {
-            await Results.Problem(new ProblemDetails
-            {
-                Title = "Endpoint not found.",
-                Type = "https://datatracker.ietf.org/doc/html/rfc7231#section-6.5.4",
-                Status = (int)HttpStatusCode.NotFound,
-                Instance = context.Request.Path,
-                Extensions = { { "traceId", Activity.Current?.Id ?? context.TraceIdentifier } }
-            }).ExecuteAsync(context);
-            return;
-        }
+        if (await HandleEdgeCases(status, context)) return;
 
         var message = GetMessageFromEndpointSummary(context, status);
-        var failures = status switch
+        var validationFailures = status switch
         {
             (int)HttpStatusCode.Forbidden => new Forbidden(message ?? "Forbidden").ToValidationResults(),
             _ => []
         };
-        var problem = context.ResponseBuilder(failures);
+        var problem = context.ResponseBuilder(validationFailures);
         if (problem == null)
         {
             Log.Error("Found no problem response for status {Code}. No body will be added.", status);
@@ -44,6 +33,27 @@ public static class UseStatusCodePagesHandlers
         }
 
         await Results.Problem(problem).ExecuteAsync(context);
+    }
+
+    private static async Task<bool> HandleEdgeCases(int status, HttpContext context)
+    {
+        switch (status)
+        {
+            case (int)HttpStatusCode.NotFound:
+                // Handle router level 404's, which means the endpoint is missing.
+                // We don't want the 404 response from FastEndpoints because those 404's = resource doesn't exist
+                await Results.Problem(new ProblemDetails
+                {
+                    Title = "Endpoint not found.",
+                    Type = "https://datatracker.ietf.org/doc/html/rfc7231#section-6.5.4",
+                    Status = (int)HttpStatusCode.NotFound,
+                    Instance = context.Request.Path,
+                    Extensions = { { "traceId", Activity.Current?.Id ?? context.TraceIdentifier } }
+                }).ExecuteAsync(context);
+                return true;
+            default:
+                return false;
+        }
     }
 
     private static string? GetMessageFromEndpointSummary(HttpContext context, int statusCode)

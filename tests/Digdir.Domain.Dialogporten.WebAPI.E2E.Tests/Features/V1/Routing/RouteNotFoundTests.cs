01631f904236ac76dd394967e664d61148e31a18
Mats Jørgensen <mats.jorgensen@digdir.no>
2026-09-18 15:35:58 +0200

    Allow /dialogporten as prefix in Instance path

--- tests/Digdir.Domain.Dialogporten.WebAPI.E2E.Tests/Features/V1/Routing/RouteNotFoundTests.cs
+++ tests/Digdir.Domain.Dialogporten.WebAPI.E2E.Tests/Features/V1/Routing/RouteNotFoundTests.cs
@@ -33,7 +33,7 @@ public class RouteNotFoundTests(WebApiE2EFixture fixture) : E2ETestBase<WebApiE2
         problemDetails.Detail.Should().BeNull();
         problemDetails.Errors.Should().NotBeNull();
         problemDetails.Errors.Keys.Count.Should().Be(0);
-        problemDetails.Instance.Should().Be(requestPath);
+        problemDetails.Instance.Should().BeOneOf(requestPath, $"/dialogporten{requestPath}");
         problemDetails.Status.Should().Be((int)HttpStatusCode.NotFound);
         problemDetails.StatusDescription.Should().BeNull();
         problemDetails.Title.Should().Be("Endpoint not found.");
